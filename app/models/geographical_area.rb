582a9efc73c543163c9e59800940c419042a2abe
Octavian Neguletu <neguletuoctavian@gmail.com>
2021-02-11 08:50:54 +0000

    Revert "Fixes issues with static service links"
    This reverts commit 53dbd31d78ec68cd28b5cb77ba432b9bd302d64a.

--- app/models/geographical_area.rb
+++ app/models/geographical_area.rb
@@ -3,7 +3,7 @@ require 'api_entity'
 class GeographicalArea
   include ApiEntity
 
-  collection_path "/geographical_areas/countries"
+  collection_path '/geographical_areas/countries'
 
   attr_accessor :id, :description, :geographical_area_id
 
@@ -19,20 +19,20 @@ class GeographicalArea
   def self.cached_countries
     TradeTariffFrontend::ServiceChooser.cache_with_service_choice(
       'cached_countries',
-      expires_in: 1.hour,
+      expires_in: 1.hour
     ) do
       countries
     end
   end
-
+  
   def self.areas
     collection('/geographical_areas').sort_by(&:id)
   end
-
+  
   def self.cached_areas
     TradeTariffFrontend::ServiceChooser.cache_with_service_choice(
       'areas',
-      expires_in: 1.hour,
+      expires_in: 1.hour
     ) do
       areas
     end
@@ -40,9 +40,9 @@ class GeographicalArea
 
   def self.by_long_description(term)
     lookup_regexp = /#{term}/i
-    cached_countries.select { |country|
+    cached_countries.select do |country|
       country.long_description =~ lookup_regexp
-    }.sort_by do |country|
+    end.sort_by do |country|
       match_id = country.id =~ lookup_regexp
       match_desc = country.description =~ lookup_regexp
       key = ''
