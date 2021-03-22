fe7ef24e112c4a3956e65a3718bc4f238d88af89
William Fish <william.michael.fish@gmail.com>
2021-03-22 11:24:31 +0000

    Removes exclusions from the frontend
    This behaviour is now managed by the backend and shared by both the
    frontend and duty calculator projects

--- spec/models/geographical_area_spec.rb
+++ spec/models/geographical_area_spec.rb
@@ -12,12 +12,6 @@ describe GeographicalArea do
     it 'sorts countries by id' do
       expect(countries.first.id).to be < countries.second.id
     end
-
-    it 'removes excluded countries (United Kingdom)' do
-      expect(
-        countries.detect { |c| c.id == 'GB' },
-      ).to be_blank
-    end
   end
 
   describe '.by_long_description', vcr: { cassette_name: 'geographical_areas#countries' } do
