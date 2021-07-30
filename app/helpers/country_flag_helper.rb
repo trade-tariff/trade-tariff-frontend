7b715bd6671c5aa701801487ec141a1c69bfd81a
jebw <jeb@jdwilkins.co.uk>
2021-07-30 18:03:44 +0100

    Added helper to add drop shadown to country flags
    Puts them in a span using CSS text-shadow

--- app/helpers/country_flag_helper.rb
+++ app/helpers/country_flag_helper.rb
@@ -1,7 +1,10 @@
 module CountryFlagHelper
-  def country_flag(iso_2_code)
+  def country_flag_emoji(iso_2_code)
     # 127_397 offsets to the appropriate unicode values
     iso_2_code.codepoints.map { |codepoint| codepoint + 127_397 }.pack('U*')
   end
+
+  def country_flag_tag(iso_2_code)
+    tag.span(country_flag_emoji(iso_2_code), class: 'country-flag')
+  end
 end
-  
--- spec/helpers/country_flag_helper_spec.rb
+++ spec/helpers/country_flag_helper_spec.rb
@@ -1,13 +1,23 @@
 require 'spec_helper'
 
 describe CountryFlagHelper, type: :helper do
-  describe "#country_flag" do
-    subject { helper.country_flag(iso_2_code) }
+  describe "#country_flag_emoji" do
+    subject { helper.country_flag_emoji(iso_2_code) }
 
     context "with GB" do
       let(:iso_2_code) { 'GB' }
 
       it { is_expected.to eql('🇬🇧') }
+    end
+  end
+
+  describe "#country_flag_tag" do
+    subject { helper.country_flag_tag(iso_2_code) }
+
+    context "with GB" do
+      let(:iso_2_code) { 'GB' }
+
+      it { is_expected.to have_css('span.country-flag', text: '🇬🇧') }
     end
   end
 end
