8d467b32c23cc3ebd5243e879cc7ef007e6c00cc
jebw <jeb@jdwilkins.co.uk>
2021-08-02 16:01:15 +0100

    Replaced iso_2_code with two_letter_country_code
    Improves code clarity

--- spec/helpers/country_flag_helper_spec.rb
+++ spec/helpers/country_flag_helper_spec.rb
@@ -2,20 +2,20 @@ require 'spec_helper'
 
 describe CountryFlagHelper, type: :helper do
   describe "#country_flag_emoji" do
-    subject { helper.country_flag_emoji(iso_2_code) }
+    subject { helper.country_flag_emoji(two_letter_country_code) }
 
     context "with GB" do
-      let(:iso_2_code) { 'GB' }
+      let(:two_letter_country_code) { 'GB' }
 
       it { is_expected.to eql('🇬🇧') }
     end
   end
 
   describe "#country_flag_tag" do
-    subject { helper.country_flag_tag(iso_2_code) }
+    subject { helper.country_flag_tag(two_letter_country_code) }
 
     context "with GB" do
-      let(:iso_2_code) { 'GB' }
+      let(:two_letter_country_code) { 'GB' }
 
       it { is_expected.to have_css('span.country-flag', text: '🇬🇧') }
     end
