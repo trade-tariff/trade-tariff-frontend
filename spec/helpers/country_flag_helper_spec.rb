5de3b7d533e034c35620a97a7608468bc153c52a
jebw <jeb@jdwilkins.co.uk>
2021-08-13 09:41:35 +0100

    RoO: Tweaked countries_flag_tag
    1. CSS tweaks
    2. Don't try to show the flag if it looks like an invalid country code

--- spec/helpers/country_flag_helper_spec.rb
+++ spec/helpers/country_flag_helper_spec.rb
@@ -1,23 +1,35 @@
 require 'spec_helper'
 
 describe CountryFlagHelper, type: :helper do
-  describe "#country_flag_emoji" do
+  describe '#country_flag_emoji' do
     subject { helper.country_flag_emoji(two_letter_country_code) }
 
-    context "with GB" do
+    context 'with GB' do
       let(:two_letter_country_code) { 'GB' }
 
       it { is_expected.to eql('🇬🇧') }
     end
+
+    context 'with non country code' do
+      let(:two_letter_country_code) { '2300' }
+
+      it { is_expected.to be_nil }
+    end
   end
 
-  describe "#country_flag_tag" do
+  describe '#country_flag_tag' do
     subject { helper.country_flag_tag(two_letter_country_code) }
 
-    context "with GB" do
+    context 'with GB' do
       let(:two_letter_country_code) { 'GB' }
 
       it { is_expected.to have_css('span.country-flag', text: '🇬🇧') }
+    end
+
+    context 'with non country code' do
+      let(:two_letter_country_code) { '2300' }
+
+      it { is_expected.to be_nil }
     end
   end
 end
