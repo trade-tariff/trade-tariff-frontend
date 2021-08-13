f99ac3d486b23590c615955657607b4000438add
jebw <jeb@jdwilkins.co.uk>
2021-08-13 12:07:33 +0100

    RoO: Only show rules of origin tab when country selected

--- spec/views/measures/_measures.html.erb_spec.rb
+++ spec/views/measures/_measures.html.erb_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
 
-describe 'measures/_measures.html.erb', type: :view do
+describe 'measures/_measures.html.erb', type: :view, vcr: {
+  cassette_name: 'geographical_areas_countries',
+} do
   subject { rendered }
 
-  before { assign :search, Search.new(q: '0102210000') }
+  before do
+    allow(GeographicalArea).to receive(:all).and_return all_countries
+    assign :search, search
+  end
+
+  let(:search) { Search.new(q: '0101300000') }
+  let(:all_countries) { GeographicalArea.all }
+  let(:presented_commodity) { CommodityPresenter.new(commodity) }
 
   let(:commodity) do
     TradeTariffFrontend::ServiceChooser.with_source(:uk) do
@@ -13,7 +22,19 @@ describe 'measures/_measures.html.erb', type: :view do
     end
   end
 
-  let(:presented_commodity) { CommodityPresenter.new(commodity) }
+  shared_examples 'measures without rules of origin tab' do
+    it { is_expected.to have_css '.govuk-tabs .govuk-tabs__tab', count: 4 }
+    it { is_expected.to have_css '.govuk-tabs__panel#overview', count: 1 }
+    it { is_expected.to have_css '.govuk-tabs__panel#import', count: 1 }
+    it { is_expected.to have_css '.govuk-tabs__panel#export', count: 1 }
+    it { is_expected.not_to have_css '.govuk-tabs__panel#rules-of-origin' }
+    it { is_expected.to have_css '.govuk-tabs__panel#footnotes', count: 1 }
+  end
+
+  shared_examples 'measures with rules of origin tab' do
+    it { is_expected.to have_css '.govuk-tabs .govuk-tabs__tab', count: 5 }
+    it { is_expected.to have_css '.govuk-tabs__panel#rules-of-origin', count: 1 }
+  end
 
   context 'with uk service' do
     before do
@@ -23,11 +44,15 @@ describe 'measures/_measures.html.erb', type: :view do
              xi_declarable: nil
     end
 
-    it { is_expected.to have_css '.govuk-tabs .govuk-tabs__tab', count: 4 }
-    it { is_expected.to have_css '.govuk-tabs__panel#overview', count: 1 }
-    it { is_expected.to have_css '.govuk-tabs__panel#import', count: 1 }
-    it { is_expected.to have_css '.govuk-tabs__panel#export', count: 1 }
-    it { is_expected.to have_css '.govuk-tabs__panel#footnotes', count: 1 }
+    context 'without country selected' do
+      it_behaves_like 'measures without rules of origin tab'
+    end
+
+    context 'with country selected' do
+      let(:search) { Search.new(q: '0101300000', 'country' => 'FR') }
+
+      it_behaves_like 'measures with rules of origin tab'
+    end
   end
 
   context 'with xi service' do
@@ -38,10 +63,14 @@ describe 'measures/_measures.html.erb', type: :view do
              xi_declarable: presented_commodity
     end
 
-    it { is_expected.to have_css '.govuk-tabs .govuk-tabs__tab', count: 4 }
-    it { is_expected.to have_css '.govuk-tabs__panel#overview', count: 1 }
-    it { is_expected.to have_css '.govuk-tabs__panel#import', count: 1 }
-    it { is_expected.to have_css '.govuk-tabs__panel#export', count: 1 }
-    it { is_expected.to have_css '.govuk-tabs__panel#footnotes', count: 1 }
+    context 'without country selected' do
+      it_behaves_like 'measures without rules of origin tab'
+    end
+
+    context 'with country selected' do
+      let(:search) { Search.new(q: '0101300000', 'country' => 'FR') }
+
+      it_behaves_like 'measures with rules of origin tab'
+    end
   end
 end
