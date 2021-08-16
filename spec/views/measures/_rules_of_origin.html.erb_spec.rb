e19c0f265758020f204381d0636e18c508f4e6fe
jebw <jeb@jdwilkins.co.uk>
2021-08-16 12:36:30 +0100

    RoO: Show introductory text on Rules tab

--- spec/views/measures/_rules_of_origin.html.erb_spec.rb
+++ spec/views/measures/_rules_of_origin.html.erb_spec.rb
@@ -7,6 +7,7 @@ describe 'measures/_rules_of_origin.html.erb', type: :view do
     render 'measures/rules_of_origin',
            country_code: 'FR',
            country_name: 'France',
+           commodity_code: '2203000100',
            rules: rules
   end
 
@@ -19,5 +20,31 @@ describe 'measures/_rules_of_origin.html.erb', type: :view do
 
   it 'shows the flag' do
     expect(rendered_page).to have_css 'h2 span.country-flag', text: '🇫🇷'
+  end
+
+  it 'references the commodity code' do
+    expect(rendered_page).to have_css 'h3', text: /for commodity 2203000100/
+  end
+
+  context 'with UK service' do
+    before do
+      allow(TradeTariffFrontend::ServiceChooser).to receive(:service_choice).and_return('uk')
+    end
+
+    it 'references the country in the introductory text' do
+      expect(rendered_page).to \
+        have_css 'p', text: /originating in the UK or France/
+    end
+  end
+
+  context 'with XI service' do
+    before do
+      allow(TradeTariffFrontend::ServiceChooser).to receive(:service_choice).and_return('xi')
+    end
+
+    it 'references the country in the introductory text' do
+      expect(rendered_page).to \
+        have_css 'p', text: /originating in the EU or France/
+    end
   end
 end
