3435bbeaf9db0eb40b47d8297201e4cd0d22f436
jebw <jeb@jdwilkins.co.uk>
2021-08-16 11:06:58 +0100

    RoO: Still show the Rules tab when no country
    But with placeholder content

--- spec/views/measures/_measures.html.erb_spec.rb
+++ spec/views/measures/_measures.html.erb_spec.rb
@@ -22,6 +22,15 @@ describe 'measures/_measures.html.erb', type: :view, vcr: {
     end
   end
 
+  shared_examples 'measures with rules of origin tab' do
+    it { is_expected.to have_css '.govuk-tabs .govuk-tabs__tab', count: 5 }
+    it { is_expected.to have_css '.govuk-tabs__panel#overview', count: 1 }
+    it { is_expected.to have_css '.govuk-tabs__panel#import', count: 1 }
+    it { is_expected.to have_css '.govuk-tabs__panel#export', count: 1 }
+    it { is_expected.to have_css '.govuk-tabs__panel#rules-of-origin', count: 1 }
+    it { is_expected.to have_css '.govuk-tabs__panel#footnotes', count: 1 }
+  end
+
   shared_examples 'measures without rules of origin tab' do
     it { is_expected.to have_css '.govuk-tabs .govuk-tabs__tab', count: 4 }
     it { is_expected.to have_css '.govuk-tabs__panel#overview', count: 1 }
@@ -29,11 +38,6 @@ describe 'measures/_measures.html.erb', type: :view, vcr: {
     it { is_expected.to have_css '.govuk-tabs__panel#export', count: 1 }
     it { is_expected.not_to have_css '.govuk-tabs__panel#rules-of-origin' }
     it { is_expected.to have_css '.govuk-tabs__panel#footnotes', count: 1 }
-  end
-
-  shared_examples 'measures with rules of origin tab' do
-    it { is_expected.to have_css '.govuk-tabs .govuk-tabs__tab', count: 5 }
-    it { is_expected.to have_css '.govuk-tabs__panel#rules-of-origin', count: 1 }
   end
 
   context 'with uk service' do
@@ -45,22 +49,24 @@ describe 'measures/_measures.html.erb', type: :view, vcr: {
     end
 
     context 'without country selected' do
-      it_behaves_like 'measures without rules of origin tab'
+      it_behaves_like 'measures with rules of origin tab'
+      it { is_expected.to have_css '#rules-of-origin h2', text: 'Rules of origin' }
     end
 
     context 'with country selected' do
       let(:search) { Search.new(q: '0101300000', 'country' => 'FR') }
 
       it_behaves_like 'measures with rules of origin tab'
+      it { is_expected.to have_css '#rules-of-origin h2', text: /Rules of origin for trading/ }
+    end
 
-      context 'with RULES_OF_ORIGIN_ENABLED set to false' do
-        before do
-          allow(TradeTariffFrontend).to \
-            receive(:rules_of_origin_enabled?).and_return false
-        end
-
-        it_behaves_like 'measures without rules of origin tab'
+    context 'with RULES_OF_ORIGIN_ENABLED set to false' do
+      before do
+        allow(TradeTariffFrontend).to \
+          receive(:rules_of_origin_enabled?).and_return false
       end
+
+      it_behaves_like 'measures without rules of origin tab'
     end
   end
 
@@ -73,22 +79,24 @@ describe 'measures/_measures.html.erb', type: :view, vcr: {
     end
 
     context 'without country selected' do
-      it_behaves_like 'measures without rules of origin tab'
+      it_behaves_like 'measures with rules of origin tab'
+      it { is_expected.to have_css '#rules-of-origin h2', text: 'Rules of origin' }
     end
 
     context 'with country selected' do
       let(:search) { Search.new(q: '0101300000', 'country' => 'FR') }
 
       it_behaves_like 'measures with rules of origin tab'
+      it { is_expected.to have_css '#rules-of-origin h2', text: /Rules of origin for trading/ }
+    end
 
-      context 'with RULES_OF_ORIGIN_ENABLED set to false' do
-        before do
-          allow(TradeTariffFrontend).to \
-            receive(:rules_of_origin_enabled?).and_return false
-        end
-
-        it_behaves_like 'measures without rules of origin tab'
+    context 'with RULES_OF_ORIGIN_ENABLED set to false' do
+      before do
+        allow(TradeTariffFrontend).to \
+          receive(:rules_of_origin_enabled?).and_return false
       end
+
+      it_behaves_like 'measures without rules of origin tab'
     end
   end
 end
