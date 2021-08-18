5e0c4428421e3eafe59cd9a1db6b7fc81d564e71
jebw <jeb@jdwilkins.co.uk>
2021-08-18 10:58:58 +0100

    RoO: Use a mocked response for now

--- app/models/product_specific_rule.rb
+++ app/models/product_specific_rule.rb
@@ -9,10 +9,30 @@ class ProductSpecificRule
 
   class << self
     def all(heading_code, country_code, opts = {})
+      return mocked_response unless Rails.env.test?
+
       super opts.merge(
         heading_code: heading_code,
         country_code: country_code,
       )
+    end
+
+  private
+
+    def mocked_response
+      (1..6).map do |i|
+        new \
+          heading: "Chapter #{i}",
+          description: 'Beverages',
+          rule: <<~RULE
+            Production from non originating materials, of any heading, provided
+            that:
+
+            * all materials from Chapter 4 are wholly owned
+            * the total weight of non-originating materials does not exceed 20%
+              of the weight of the product
+          RULE
+      end
     end
   end
 end
