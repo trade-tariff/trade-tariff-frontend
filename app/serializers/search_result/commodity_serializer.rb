178d15f9c3d7ae6d2080655f07ff17f26f1bedcb
William Fish <william.michael.fish@gmail.com>
2021-09-03 15:30:50 +0100

    Tidy serializers

--- app/serializers/search_result/commodity_serializer.rb
+++ app/serializers/search_result/commodity_serializer.rb
@@ -3,14 +3,14 @@ module SearchResult
     include ActiveModel::Serializers::JSON
     def serializable_hash(_opts = {})
       {
-        type: 'commodity',
-        goods_nomenclature_item_id: goods_nomenclature_item_id,
-        producline_suffix: producline_suffix,
-        number_indents: number_indents,
         declarable: declarable,
-        validity_start_date: validity_start_date.to_s,
-        validity_end_date: validity_end_date.to_s,
         description: description,
+        goods_nomenclature_item_id: goods_nomenclature_item_id,
+        number_indents: number_indents,
+        producline_suffix: producline_suffix,
+        type: 'commodity',
+        validity_end_date: validity_end_date.to_s,
+        validity_start_date: validity_start_date.to_s,
       }
     end
   end
--- app/serializers/search_result/heading_serializer.rb
+++ app/serializers/search_result/heading_serializer.rb
@@ -1,16 +1,17 @@
 module SearchResult
   class HeadingSerializer < SimpleDelegator
     include ActiveModel::Serializers::JSON
+
     def serializable_hash(_opts = {})
       {
-        type: 'heading',
-        goods_nomenclature_item_id: goods_nomenclature_item_id,
-        producline_suffix: producline_suffix,
-        number_indents: number_indents,
-        validity_start_date: validity_start_date.to_s,
-        validity_end_date: validity_end_date.to_s,
-        description: description,
         declarable: declarable,
+        description: description,
+        goods_nomenclature_item_id: goods_nomenclature_item_id,
+        number_indents: number_indents,
+        producline_suffix: producline_suffix,
+        type: 'heading',
+        validity_end_date: validity_end_date.to_s,
+        validity_start_date: validity_start_date.to_s,
       }
     end
   end
