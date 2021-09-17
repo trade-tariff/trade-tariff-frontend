ce3852ab9aa2c6680e566021d4d040c64d22d6d9
jebw <jeb@jdwilkins.co.uk>
2021-09-17 09:42:14 +0100

    HOTT-846: Dropped rules API call support
    Since there's not a direct rules api endpoint anymore

--- app/models/rules_of_origin/rule.rb
+++ app/models/rules_of_origin/rule.rb
@@ -4,13 +4,4 @@ class RulesOfOrigin::Rule
   include ApiEntity
 
   attr_accessor :id_rule, :heading, :description, :rule
-
-  class << self
-    def all(heading_code, country_code, opts = {})
-      super opts.merge(
-        heading_code: heading_code.first(6),
-        country_code: country_code,
-      )
-    end
-  end
 end
