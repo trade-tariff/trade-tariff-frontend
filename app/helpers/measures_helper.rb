e7bb4ea4e841147b1573077ce66cfa0978133b7c
William Fish <william.michael.fish@gmail.com>
2021-09-28 12:50:18 +0100

    WIP modal modal modal

--- app/helpers/measures_helper.rb
+++ app/helpers/measures_helper.rb
@@ -1,4 +1,33 @@
 module MeasuresHelper
+  def modal_partial_options_for(declarable, measure)
+    config = config_for(
+      declarable.goods_nomenclature_item_id,
+      measure.additional_code.code,
+      measure.measure_type.id,
+    )
+
+    overwrite = config['overwrite']
+    content_file = config['content_file']
+
+    if content_file.present?
+      if overwrite
+        { partial: "measure_condition_modals/#{content_file}" }
+      else
+        { partial: 'measure_condition_modal_augment', locals: { modal_content_file: measure_condition.content_file } }
+      end
+    else
+      { partial: 'measure_condition_modal_default', locals: { measure: measure } }
+    end
+  end
+
+  def config_for(goods_nomenclature_item_id, additional_code, measure_type_id)
+    TradeTariffFrontend.measure_condition_modal_config.find do |modal_config|
+      modal_config[goods_nomenclature_item_id] == goods_nomenclature_item_id &&
+        modal_config[additional_code] == additional_code &&
+        modal_config[measure_type_id] == measure_type_id
+    end
+  end
+
   def filter_duty_expression(measure)
     duty_expression = measure.duty_expression.to_s.html_safe
     duty_expression = '' if duty_expression == 'NIHIL'
