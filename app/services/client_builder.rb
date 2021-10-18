31cc6e8793eabca20839e0ecd20efbae05538cd5
William Fish <william.michael.fish@gmail.com>
2021-10-18 13:11:21 +0100

    Use net_http_persistent adapter

--- app/services/client_builder.rb
+++ app/services/client_builder.rb
@@ -9,17 +9,28 @@ class ClientBuilder
 
   def call
     return nil if TradeTariffFrontend::ServiceChooser.service_choices.blank?
-    return Faraday.new(host) if @forwarding
 
+    return forwarding_client if @forwarding
+
+    resource_client
+  end
+
+  private
+
+  def forwarding_client
+    Faraday.new(host) do |conn|
+      conn.adapter :net_http_persistent
+    end
+  end
+
+  def resource_client
     Faraday.new(host) do |conn|
       conn.request :url_encoded
-      conn.adapter Faraday.default_adapter
+      conn.adapter :net_http_persistent
       conn.response :json, content_type: /\bjson$/
       conn.headers['Accept'] = "application/vnd.uktt.v#{DEFAULT_VERSION}"
     end
   end
-
-  private
 
   def host
     TradeTariffFrontend::ServiceChooser.public_send("#{@service}_host")
