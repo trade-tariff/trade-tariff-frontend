5dda0bee74de18f59f974edbbc4bcc0e0230b417
jebw <jeb@jdwilkins.co.uk>
2021-10-19 15:28:45 +0100

    HOTT-1045: Add specs around api client

---
+++ spec/lib/tariff_jsonapi_parser_spec.rb
@@ -0,0 +1,73 @@
+require 'spec_helper'
+
+RSpec.describe TariffJsonapiParser do
+  describe '#parse' do
+    let(:json) { JSON.parse file_fixture("jsonapi/#{json_file}.json").read }
+
+    context 'with singular resource' do
+      subject(:parse) { described_class.new(json).parse }
+
+      context 'with valid' do
+        let(:json_file) { 'singular_no_relationship' }
+
+        it { is_expected.to include 'name' => 'Joe' }
+        it { is_expected.to include 'age' => 21 }
+      end
+
+      context 'with relationships' do
+        let(:json_file) { 'singular_with_relationship' }
+
+        it { is_expected.to include 'name' => 'Joe' }
+        it { is_expected.to include 'age' => 21 }
+        it { is_expected.to include 'parts' => [{ 'part_name' => 'A part name' }] }
+      end
+
+      context 'with missing relationships' do
+        let(:json_file) { 'singular_missing_relationship' }
+
+        it { is_expected.to include 'name' => 'Joe' }
+        it { is_expected.to include 'age' => 21 }
+        it { is_expected.to include 'parts' => [{}] }
+      end
+
+      context 'with invalid relationships' do
+        let(:json_file) { 'singular_invalid_relationship' }
+
+        it { expect { parse }.to raise_exception NoMethodError }
+      end
+    end
+
+    context 'with array resource' do
+      subject(:parse) { described_class.new(json).parse.first }
+
+      context 'with valid' do
+        let(:json_file) { 'multiple_no_relationship' }
+
+        it { is_expected.to include 'name' => 'Joe' }
+        it { is_expected.to include 'age' => 21 }
+      end
+
+      context 'with relationships' do
+        let(:json_file) { 'multiple_with_relationship' }
+
+        it { is_expected.to include 'name' => 'Joe' }
+        it { is_expected.to include 'age' => 21 }
+        it { is_expected.to include 'parts' => [{ 'part_name' => 'A part name' }] }
+      end
+
+      context 'with missing relationships' do
+        let(:json_file) { 'multiple_missing_relationship' }
+
+        it { is_expected.to include 'name' => 'Joe' }
+        it { is_expected.to include 'age' => 21 }
+        it { is_expected.to include 'parts' => [{}] }
+      end
+
+      context 'with invalid relationships' do
+        let(:json_file) { 'multiple_invalid_relationship' }
+
+        it { expect { parse }.to raise_exception NoMethodError }
+      end
+    end
+  end
+end
