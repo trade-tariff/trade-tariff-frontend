115e2f0b7aba83517a949d2c6b2157aa97b24099
jebw <jeb@jdwilkins.co.uk>
2021-11-05 16:29:35 +0000

    HOTT-1097 test loading of accessible autocomplete
    Use Jests own JSDOM integration - seems to solve some issues with testing

--- spec/javascript/commodities.spec.js
+++ spec/javascript/commodities.spec.js
@@ -1,6 +1,7 @@
 // There are tests for commodities.js
 // To run them: 'yarn test' .
 
+global.accessibleAutocomplete = require('accessible-autocomplete');
 require('commodities.js');
 
 test('GOVUK.tariff public has expected public methods', () => {
@@ -21,5 +22,32 @@ test('GOVUK.tariff public has expected public methods', () => {
 
   items.forEach( (item) => {
     expect(GOVUK.tariff).toHaveProperty(item);
+  });
+});
+
+describe('searchForm', () => {
+  const autocompleteSelector = '.autocomplete__wrapper input';
+
+  const commodityPickerTemplate = `
+    <div class="search-header js-search-header">
+      <input type="text" name="q" class="js-commodity-picker-target" />
+      <div class='js-commodity-picker-select js-show'></div>
+
+      <input type="input" name="day" id="tariff_date_day" />
+      <input type="input" name="month" id="tariff_date_month" />
+      <input type="input" name="year" id="tariff_date_year" />
+
+      <input type="submit" value="Search" />
+    </div>
+  `;
+
+  beforeEach(() => {
+    document.body.innerHTML = commodityPickerTemplate;
+    GOVUK.tariff.selectBoxes.initialize(document.body);
+  });
+
+  it('loads the autocomplete', () => {
+    const autocompleteWidgets = document.querySelectorAll(autocompleteSelector);
+    expect(autocompleteWidgets.length).toBe(1);
   });
 });
