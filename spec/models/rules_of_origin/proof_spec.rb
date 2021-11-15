d1d94b07b1f7584d427193dae44b601f94b33d8e
William Fish <william.michael.fish@gmail.com>
2021-11-15 16:25:51 +0000

    Namespace all describes

--- spec/models/rules_of_origin/proof_spec.rb
+++ spec/models/rules_of_origin/proof_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe RulesOfOrigin::Proof do
+RSpec.describe RulesOfOrigin::Proof do
   it { is_expected.to respond_to :summary }
   it { is_expected.to respond_to :url }
   it { is_expected.to respond_to :subtext }
