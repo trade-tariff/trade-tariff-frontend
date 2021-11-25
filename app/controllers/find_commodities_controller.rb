937e7dded03000f034126d76053efe04144926e9
Alexdesi <alexdesi@gmail.com>
2021-11-25 10:45:39 +0000

    Remove switch banner for the find commotidies page (the Home page)

--- app/controllers/find_commodities_controller.rb
+++ app/controllers/find_commodities_controller.rb
@@ -1,4 +1,6 @@
 class FindCommoditiesController < ApplicationController
+  before_action :disable_switch_service_banner, only: [:show]
+
   def show
     @no_shared_search = true
     @latest_news = NewsItem.latest_for_home_page
