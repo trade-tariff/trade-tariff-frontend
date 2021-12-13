ac3e4c22311d7f0879a7a1f72261f12bc44af143
William Fish <william.michael.fish@gmail.com>
2021-12-13 10:16:33 +0000

    Removes unused method

--- app/presenters/declarable_presenter.rb
+++ app/presenters/declarable_presenter.rb
@@ -1,12 +1,6 @@
 class DeclarablePresenter < TradeTariffFrontend::Presenter
-  MEURSING_TOOL_LINK = 'http://ec.europa.eu/taxation_customs/dds2/taric/measures.jsp?Lang=en&SimDate=%{date}&Taric=%{commodity_code}&LangDescr=en'.freeze
-
   def to_s
     formatted_description.html_safe
-  end
-
-  def meursing_tool_link_for(date)
-    sprintf(MEURSING_TOOL_LINK, date: date, commodity_code: code)
   end
 
   def self.model_name
