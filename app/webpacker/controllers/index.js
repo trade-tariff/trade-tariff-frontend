61b84040e4f845a65aef1b15b532e520c4bf0700
jebw <jeb@jdwilkins.co.uk>
2021-12-16 18:28:03 +0000

    HOTT-1171 Upgraded to Stimulus 3
    * Fixed transpiling stimulus 3 for NodeJS
    * Enable Stimulus 3 debug in development

--- app/webpacker/controllers/index.js
+++ app/webpacker/controllers/index.js
@@ -1,9 +1,13 @@
 // Load all the controllers within this directory and all subdirectories. 
 // Controller files must be named *_controller.js.
 
+import '@stimulus/polyfills'
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
+
+// enable Stimulus debug mode in development
+application.debug = process.env.NODE_ENV === 'development'
