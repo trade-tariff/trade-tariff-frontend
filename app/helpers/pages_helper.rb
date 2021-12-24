6da20b06181be518d5c41c045421036127315e96
William Fish <william.michael.fish@gmail.com>
2021-12-24 10:29:51 +0000

    Updates to the CN2022 page

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,16 +1,4 @@
 module PagesHelper
-  def cn2021_cn2022_10_digit_files(chapters)
-    Dir.glob('public/cn2021_2022/2022 10 digit codes*').map do |file|
-      asset              = file.gsub('public/', '')
-      asset_size         = number_to_human_size(File.size(file))
-      chapter_short_code = file.match(%r{Chapter (?<chapter_id>\d{2})})[:chapter_id]
-      chapter            = chapter_for(chapter_short_code, chapters)
-      link_text          = "Chapter #{chapter_short_code} - #{chapter} (2022 10-digit changes)"
-
-      [asset, asset_size, link_text]
-    end
-  end
-
   def cn2021_cn2022_8_digit_file
     file = 'public/cn2021_2022/CorrelationCN2021toCN2022Rev21Oct.xls'
     asset = 'cn2021_2022/CorrelationCN2021toCN2022Rev21Oct.xls'
--- spec/helpers/pages_helper_spec.rb
+++ spec/helpers/pages_helper_spec.rb
@@ -1,20 +1,6 @@
 require 'spec_helper'
 
 RSpec.describe PagesHelper, type: :helper do
-  describe '#cn2021_cn2022_10_digit_files', vcr: { cassette_name: 'chapters' } do
-    subject(:cn2021_cn2022_10_digit_files) { helper.cn2021_cn2022_10_digit_files(Chapter.all) }
-
-    let(:expected_file_info) do
-      [
-        'cn2021_2022/2022 10 digit codes Chapter 03.xlsx',
-        '13 KB',
-        'Chapter 03 - Fish and crustaceans, molluscs and other aquatic invertebrates (2022 10-digit changes)',
-      ]
-    end
-
-    it { is_expected.to include(expected_file_info) }
-  end
-
   describe '#cn2021_cn2022_8_digit_file' do
     subject(:cn2021_cn2022_8_digit_file) { helper.cn2021_cn2022_8_digit_file }
 
