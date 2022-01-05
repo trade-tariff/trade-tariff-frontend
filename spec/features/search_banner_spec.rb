310bda3046fe3012786b4c10cce4380e0b0e8a85
jebw <jeb@jdwilkins.co.uk>
2022-01-05 10:17:37 +0000

    HOTT-1171 Renamed hott-search-banner to tariff-search-banner
    Addresses review comments

--- spec/features/search_banner_spec.rb
+++ spec/features/search_banner_spec.rb
@@ -10,23 +10,23 @@ RSpec.describe 'Search banner', js: true do
           VCR.use_cassette('search#gold') do
             visit browse_sections_path
 
-            page.find('.hott-search-banner__toggle').click
+            page.find('.tariff-search-banner__toggle').click
 
-            page.find('.hott-search-banner .autocomplete__input').click
+            page.find('.tariff-search-banner .autocomplete__input').click
 
-            page.find('.hott-search-banner .autocomplete__input').set('gold')
+            page.find('.tariff-search-banner .autocomplete__input').set('gold')
             sleep 1
 
-            expect(page.find('.hott-search-banner .autocomplete__option--focused').text).to eq('gold')
+            expect(page.find('.tariff-search-banner .autocomplete__option--focused').text).to eq('gold')
 
             using_wait_time 1 do
-              expect(page.find_all('.hott-search-banner .autocomplete__option').length).to be > 1
+              expect(page.find_all('.tariff-search-banner .autocomplete__option').length).to be > 1
             end
 
-            expect(page.find('.hott-search-banner .autocomplete__option--focused').text).to eq('gold')
+            expect(page.find('.tariff-search-banner .autocomplete__option--focused').text).to eq('gold')
             expect(page).to have_content('gold - gold coin')
 
-            page.find('.hott-search-banner .autocomplete__option--focused').click
+            page.find('.tariff-search-banner .autocomplete__option--focused').click
 
             # trying to see if redirect done by JS needs some sleep to be caught up
             sleep 1
@@ -45,19 +45,19 @@ RSpec.describe 'Search banner', js: true do
           VCR.use_cassette('search#gibberish') do
             visit browse_sections_path
 
-            page.find('.hott-search-banner__toggle').click
+            page.find('.tariff-search-banner__toggle').click
 
-            page.find('.hott-search-banner .autocomplete__input').click
+            page.find('.tariff-search-banner .autocomplete__input').click
 
-            page.find('.hott-search-banner .autocomplete__input').set('dsauidoasuiodsa')
+            page.find('.tariff-search-banner .autocomplete__input').set('dsauidoasuiodsa')
 
             sleep 1
 
-            expect(page.find_all('.hott-search-banner .autocomplete__option').length).to eq(1)
-            expect(page.find('.hott-search-banner .autocomplete__option--focused').text).to eq('dsauidoasuiodsa')
+            expect(page.find_all('.tariff-search-banner .autocomplete__option').length).to eq(1)
+            expect(page.find('.tariff-search-banner .autocomplete__option--focused').text).to eq('dsauidoasuiodsa')
             sleep 1
 
-            page.find('.hott-search-banner .autocomplete__option--focused').click
+            page.find('.tariff-search-banner .autocomplete__option--focused').click
 
             # trying to see if redirect done by JS needs some sleep to be caught up
             sleep 1
