d283250dc741be013b52a9a4f11e0ea25a0f3a6c
jebw <jeb@jdwilkins.co.uk>
2022-02-28 09:15:01 +0000

    Revert "Merge pull request #660 from trade-tariff/HOTT-1279-search-redirects"
    This reverts commit 436306831c5a242675f15a159a3d47f9688f5e18, reversing
    changes made to 799df5fd63410b9bce46136526e00bd4b3738541.

--- app/models/search.rb
+++ app/models/search.rb
@@ -3,9 +3,6 @@ require 'api_entity'
 class Search
   include ApiEntity
 
-  COMMODITY_CODE = /\A[0-9]{10}\z/
-  HEADING_CODE = /\A[0-9]{4}\z/
-
   attr_reader   :q        # search text query
   attr_accessor :country, # search country
                 :day,
@@ -36,7 +33,7 @@ class Search
   end
 
   def q=(term)
-    @q = term.to_s.gsub(/(\[|\])/, '').strip
+    @q = term.to_s.gsub(/(\[|\])/, '')
   end
 
   def countries
@@ -75,14 +72,6 @@ class Search
 
   def missing_search_term?
     !contains_search_term?
-  end
-
-  def search_term_is_commodity_code?
-    COMMODITY_CODE.match? q
-  end
-
-  def search_term_is_heading_code?
-    HEADING_CODE.match? q
   end
 
   def query_attributes
