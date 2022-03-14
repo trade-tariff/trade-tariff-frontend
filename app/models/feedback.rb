4cffdac3937c394fa58f396331c2b6a481ef6bda
Alexdesi <alexdesi@gmail.com>
2022-03-14 17:02:02 +0000

    HOTT-1384 Add validations to Feedback.

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -6,5 +6,7 @@ class Feedback
   attr_accessor :message, :name, :email
 
   validates :message, presence: true
+  validates :message, length: { minimum: 10, maximum: 500 }
+
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_blank: true
 end
