15ed268543f9cc4c804785ed24954cff5da67026
Alexdesi <alexdesi@gmail.com>
2022-05-05 15:37:51 +0100

    Fix rubocop suggestions.

--- spec/models/search_reference_spec.rb
+++ spec/models/search_reference_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 RSpec.describe SearchReference do
-  subject { build :search_reference, :with_subheading }
+  subject(:search_reference) { build :search_reference, :with_subheading }
 
-    it 'has correct attributes' do
-      is_expected.to have_attributes(id: '1',
-        title: 'tomatoes',
-        referenced_id: '1234567890',
-        referenced_class: 'Subheading',
-        productline_suffix: '12')
-    end
+  it 'has correct attributes' do
+    expect(search_reference).to have_attributes(id: '1',
+                                                title: 'tomatoes',
+                                                referenced_id: '1234567890',
+                                                referenced_class: 'Subheading',
+                                                productline_suffix: '12')
+  end
 end
