dfe5019af61165d81761d9b756be57956415e9b2
William Fish <william.michael.fish@gmail.com>
2022-05-17 11:23:13 +0100

    Disable RSpec dsl in main
    This is consistent with the rails generators and it is generally more
    desirable to do things in one way, only:
    
    https://github.com/rspec/rspec-rails/commit/ca0d249858903949052e06884e8e7f9d596cdc79

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -49,6 +49,7 @@ RSpec.configure do |config|
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
   config.file_fixture_path = 'spec/fixtures'
+  config.expose_dsl_globally = false
 
   config.include FactoryBot::Syntax::Methods
   config.include Rails.application.routes.url_helpers
