0de490d8a62b6965779a0b2a62c0fde87c93c85a
Alexdesi <alexdesi@gmail.com>
2022-05-20 16:40:07 +0100

    HOTT-1567: Make use of redis_cache_store instead of outdated redis_store.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   # Use a different cache store in production.
-  config.cache_store = :redis_store,
+  config.cache_store = :redis_cache_store,
                        PaasConfig.redis.merge({
                          expires_in: 1.day,
                          namespace: ENV['GOVUK_APP_DOMAIN'],
