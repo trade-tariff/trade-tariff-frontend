c0c07848613ca330bde32a2f469a0d4374c04cf9
jebw <jeb@jdwilkins.co.uk>
2022-05-30 11:44:21 +0100

    HOTT-1638 Display legend correctly

---
+++ config/initializers/govuk_design_system_formbuilder.rb
@@ -0,0 +1,5 @@
+GOVUKDesignSystemFormBuilder.configure do |conf|
+  conf.default_legend_tag           = 'h1'
+  conf.default_legend_size          = 'xl'
+  conf.default_caption_size         = 'xl'
+end
