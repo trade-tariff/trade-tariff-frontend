e4f09cde8ca2c267db2825af8e4f198097175e95
jebw <jeb@jdwilkins.co.uk>
2022-05-30 14:32:04 +0100

    HOTT-1638 Initialize wizard state

--- app/controllers/rules_of_origin/steps_controller.rb
+++ app/controllers/rules_of_origin/steps_controller.rb
@@ -11,6 +11,14 @@ module RulesOfOrigin
       disable_switch_service_banner
     end
 
+    before_action :check_service_hasnt_changed, except: :index
+
+    def index
+      initialize_wizard!
+
+      super
+    end
+
     private
 
     def wizard_store_key
@@ -19,6 +27,27 @@ module RulesOfOrigin
 
     def step_path(step_id = params[:id])
       rules_of_origin_step_path(step_id)
+    end
+
+    def initialize_wizard!
+      wizard_store.purge!
+      wizard_store.persist service: TradeTariffFrontend::ServiceChooser.service_name,
+                           country_code: params[:country_code],
+                           commodity_code: params[:commodity_code]
+    end
+
+    def check_service_hasnt_changed
+      return if TradeTariffFrontend::ServiceChooser.service_name == wizard_store['service']
+
+      if wizard_store['commodity_code'].present?
+        redirect_to commodity_path(wizard_store['commodity_code'],
+                                   country: wizard_store['country_code'],
+                                   anchor: 'rules-of-origin')
+      else
+        redirect_to find_commodity_path
+      end
+
+      wizard_store.purge!
     end
   end
 end
