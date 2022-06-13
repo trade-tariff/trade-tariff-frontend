aca55cdf5bcd5d10b267732f6a65d103d5cec316
jebw <jeb@jdwilkins.co.uk>
2022-06-13 12:50:07 +0100

    HOTT-1673 Don't set unknown locales
    The locale param can be set via the page path or the query params
    
    Whilst the page path naturally filters the locale by the available
    locales, the query param approach does not so added manual filtering to
    handle this scenario

--- spec/features/locale_spec.rb
+++ spec/features/locale_spec.rb
@@ -17,6 +17,18 @@ RSpec.describe 'Page banner' do
     it { is_expected.to have_css 'header.govuk-header a', text: 'UK Integrated Online Tariff in Welsh' }
   end
 
+  context 'with invalid locale prefix' do
+    before { visit '/fr/find_commodity' }
+
+    it { is_expected.to have_css 'header.govuk-header a', text: 'UK Integrated Online Tariff' }
+  end
+
+  context 'with invalid locale param' do
+    before { visit '/find_commodity?locale=fr' }
+
+    it { is_expected.to have_css 'header.govuk-header a', text: 'UK Integrated Online Tariff' }
+  end
+
   context 'with service prefix' do
     before { visit '/xi/find_commodity' }
 
