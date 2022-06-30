65796a2a91c18b923aa2aa3b4db936c420c476de
jebw <jeb@jdwilkins.co.uk>
2022-06-30 16:36:14 +0100

    HOTT-1640 Addressed review comments
    - Pushed containing html tag into translation file

--- spec/views/rules_of_origin/steps/_sidebar_section.html.erb_spec.rb
+++ spec/views/rules_of_origin/steps/_sidebar_section.html.erb_spec.rb
@@ -40,7 +40,7 @@ RSpec.describe 'rules_of_origin/steps/_sidebar_section', type: :view do
              current_step:
     end
 
-    it { is_expected.to have_css 'p.app-step-nav__paragraph', text: /commodity #{commcode} from #{country.description}/m }
+    it { is_expected.to have_css '.app-step-nav__panel > p', text: /commodity #{commcode} from #{country.description}/m }
     it { is_expected.to have_link commcode.to_s, href: commodity_path(commcode, country: country.id) }
   end
 end
