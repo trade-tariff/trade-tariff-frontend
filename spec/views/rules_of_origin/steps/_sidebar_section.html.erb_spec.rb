ca93726f30d192ae7b4300d3e91faa58df92c815
jebw <jeb@jdwilkins.co.uk>
2022-06-30 13:27:02 +0100

    HOTT-1640 Show link to commodity in sidebar

--- spec/views/rules_of_origin/steps/_sidebar_section.html.erb_spec.rb
+++ spec/views/rules_of_origin/steps/_sidebar_section.html.erb_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe 'rules_of_origin/steps/_sidebar_section', type: :view do
            current_step:
   end
 
+  let(:commcode) { wizardstore['commodity_code'] }
+
   it { is_expected.to have_css 'li.app-step-nav__step h3.app-step-nav__title span', text: 'Step 2' }
   it { is_expected.to have_css 'li h3 span', text: "Are your goods 'originating'?" }
 
@@ -21,11 +23,24 @@ RSpec.describe 'rules_of_origin/steps/_sidebar_section', type: :view do
 
   context 'when rendering current_step' do
     let :render_page do
-      render 'rules_of_origin/steps/sidebar_section', object: sections['originating'],
+      render 'rules_of_origin/steps/sidebar_section', object: ['originating', sections['originating']],
+                                                      sidebar_section_counter: 1,
                                                       current_step:
 
       it { is_expected.to have_css 'li.app-step-nav.app-step-nav__step--active' }
       it { is_expected.to have_css 'li ol li.app-step-nav__list-item--active' }
     end
+  end
+
+  context 'when addition info available' do
+    let :render_page do
+      render 'rules_of_origin/steps/sidebar_section',
+             sidebar_section: ['details', sections['details']],
+             sidebar_section_counter: 1,
+             current_step:
+    end
+
+    it { is_expected.to have_css 'p.app-step-nav__paragraph', text: /commodity #{commcode} from #{country.description}/m }
+    it { is_expected.to have_link commcode.to_s, href: commodity_path(commcode, country: country.id) }
   end
 end
