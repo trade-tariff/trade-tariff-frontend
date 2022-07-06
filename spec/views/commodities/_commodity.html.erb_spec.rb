a4b40b799bcde6240a6671888da69b362d6e9c61
William Fish <william.michael.fish@gmail.com>
2022-07-06 11:01:31 +0100

    HOTT-1737: Hides VAT overview measures when on XI service

--- spec/views/commodities/_commodity.html.erb_spec.rb
+++ spec/views/commodities/_commodity.html.erb_spec.rb
@@ -6,61 +6,78 @@ RSpec.describe 'commodities/_commodity', type: :view do
     rendered
   end
 
-  describe 'for commodity' do
-    context 'with children' do
-      let(:heading) { build :heading, commodities: [parent, child] }
+  let(:uk_service) { true }
+  let(:commodity) { build :commodity }
 
-      let(:parent) do
-        attributes_for :commodity, producline_suffix: producline_suffix,
-                                   number_indents: 2
+  before do
+    allow(TradeTariffFrontend::ServiceChooser).to receive(:uk?).and_return(uk_service)
+  end
+
+  context 'when on the uk service' do
+    let(:uk_service) { true }
+
+    it { is_expected.to have_css 'div.vat', count: 1 }
+  end
+
+  context 'when not on the uk service' do
+    let(:uk_service) { false }
+
+    it { is_expected.not_to have_css 'div.vat', count: 1 }
+  end
+
+  context 'when the commodity has children' do
+    let(:heading) { build :heading, commodities: [parent, child] }
+
+    let(:parent) do
+      attributes_for :commodity, producline_suffix:,
+                                 number_indents: 2
+    end
+
+    let(:child) do
+      attributes_for :commodity, producline_suffix:,
+                                 parent_sid: parent[:goods_nomenclature_sid],
+                                 number_indents: 3
+    end
+
+    let(:commodity) { heading.commodities.first }
+
+    context 'with producline_suffix of 80' do
+      let(:producline_suffix) { '80' }
+
+      it 'will show the commodity code' do
+        expect(rendered_page).to have_css \
+          'li.has_children > .sub_heading_commodity_code_block .segmented-commodity-code'
       end
 
-      let(:child) do
-        attributes_for :commodity, producline_suffix: producline_suffix,
-                                   parent_sid: parent[:goods_nomenclature_sid],
-                                   number_indents: 3
-      end
-
-      let(:commodity) { heading.commodities.first }
-
-      context 'with producline_suffix of 80' do
-        let(:producline_suffix) { '80' }
-
-        it 'will show the commodity code' do
-          expect(rendered_page).to have_css \
-            'li.has_children > .sub_heading_commodity_code_block .segmented-commodity-code'
-        end
-
-        it 'shows the children with their codes' do
-          expect(rendered_page).to have_css \
-            'li.has_children ul.govuk-list > li .commodity__info .segmented-commodity-code',
-            count: 1
-        end
-      end
-
-      context 'with producline_suffix of 10' do
-        let(:producline_suffix) { '10' }
-
-        it { is_expected.to have_css 'li.has_children > .sub_heading_commodity_code_block' }
-
-        it 'will not show the commodity code' do
-          expect(rendered_page).not_to have_css \
-            'li.has_children > .sub_heading_commodity_code_block .segmented-commodity-code'
-        end
-
-        it 'shows the children with their codes' do
-          expect(rendered_page).to have_css \
-            'li.has_children ul.govuk-list > li .commodity__info .segmented-commodity-code',
-            count: 1
-        end
+      it 'shows the children with their codes' do
+        expect(rendered_page).to have_css \
+          'li.has_children ul.govuk-list > li .commodity__info .segmented-commodity-code',
+          count: 1
       end
     end
 
-    context 'without children' do
-      let(:commodity) { build :commodity }
+    context 'with producline_suffix of 10' do
+      let(:producline_suffix) { '10' }
 
-      it { is_expected.to have_css 'li .commodity__info .segmented-commodity-code' }
-      it { is_expected.not_to have_css 'li.has_children' }
+      it { is_expected.to have_css 'li.has_children > .sub_heading_commodity_code_block' }
+
+      it 'will not show the commodity code' do
+        expect(rendered_page).not_to have_css \
+          'li.has_children > .sub_heading_commodity_code_block .segmented-commodity-code'
+      end
+
+      it 'shows the children with their codes' do
+        expect(rendered_page).to have_css \
+          'li.has_children ul.govuk-list > li .commodity__info .segmented-commodity-code',
+          count: 1
+      end
     end
+  end
+
+  context 'when the commodity does not have children' do
+    let(:commodity) { build :commodity }
+
+    it { is_expected.to have_css 'li .commodity__info .segmented-commodity-code' }
+    it { is_expected.not_to have_css 'li.has_children' }
   end
 end
