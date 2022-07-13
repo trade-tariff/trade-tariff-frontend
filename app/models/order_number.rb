cce3580cd0cc949d94ddd4d946aada7f2f636d94
William Fish <william.michael.fish@gmail.com>
2022-07-13 10:35:15 +0100

    HOTT-1752: Adds convenients methods to order number model

--- app/models/order_number.rb
+++ app/models/order_number.rb
@@ -2,6 +2,12 @@ require 'api_entity'
 require 'order_number/definition'
 
 class OrderNumber
+  ORDER_NUMBERS_WITH_KNOWN_ISSUES = %w[
+    052012
+    052105
+    052106
+  ].freeze
+
   include ApiEntity
 
   attr_accessor :number
@@ -19,5 +25,27 @@ class OrderNumber
   def definition
     return @definition if @definition
     return casted_by if casted_by.is_a?(OrderNumber::Definition)
+  end
+
+  def warning_text
+    if known_data_issues?
+      I18n.t('quota_definition.order_number.known_issues')
+    else
+      definition.description
+    end
+  end
+
+  def show_warning?
+    starts_with_05? || known_data_issues?
+  end
+
+  private
+
+  def starts_with_05?
+    definition.description && number.start_with?('05')
+  end
+
+  def known_data_issues?
+    number.in?(ORDER_NUMBERS_WITH_KNOWN_ISSUES)
   end
 end
