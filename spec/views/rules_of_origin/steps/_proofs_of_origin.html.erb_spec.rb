2fd5c5f350979a76c2eaf0bd0e2a9200adf01c0b
jebw <jeb@jdwilkins.co.uk>
2022-07-14 12:27:47 +0100

    HOTT-1709 Added valid proofs of origin step

---
+++ spec/views/rules_of_origin/steps/_proofs_of_origin.html.erb_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+RSpec.describe 'rules_of_origin/steps/_origin_requirements_met', type: :view do
+  include_context 'with rules of origin form step',
+                  'proofs_of_origin',
+                  :wholly_obtained
+
+  it { is_expected.to have_css 'span.govuk-caption-xl', text: %r{(Im|Ex)porting.* #{wizardstore['commodity_code']}.*Japan} }
+  it { is_expected.to have_css 'h1', text: /valid proofs/i }
+  it { is_expected.to have_css 'p', text: /preferential tariff/ }
+  it { is_expected.to have_css '#proofs-overview h3', text: /overview/ }
+  it { is_expected.to have_css '#proofs-overview p', text: /provide proof/ }
+  it { is_expected.to have_css '#proofs-overview a', count: 2 }
+  it { is_expected.to have_css 'h3', text: /Next/i }
+  it { is_expected.to have_css '#next-steps a', count: 2 }
+end
