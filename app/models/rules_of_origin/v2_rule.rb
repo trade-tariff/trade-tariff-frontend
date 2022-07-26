789931e449463b63836dbe3f098a40093c4da8b2
jebw <jeb@jdwilkins.co.uk>
2022-07-26 10:44:05 +0100

    HOTT-1779 Added test for Wholly obtained class
    To v2 rule

--- app/models/rules_of_origin/v2_rule.rb
+++ app/models/rules_of_origin/v2_rule.rb
@@ -3,5 +3,16 @@ require 'api_entity'
 class RulesOfOrigin::V2Rule
   include ApiEntity
 
-  attr_accessor :rule, :rule_class, :operator
+  WHOLLY_OBTAINED_CLASS = 'WO'.freeze
+
+  attr_accessor :rule, :operator
+  attr_writer :rule_class
+
+  def rule_class
+    @rule_class ||= []
+  end
+
+  def wholly_obtained_class?
+    rule_class.include?(WHOLLY_OBTAINED_CLASS)
+  end
 end
