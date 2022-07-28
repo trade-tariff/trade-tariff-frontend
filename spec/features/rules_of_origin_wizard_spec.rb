f13a783b48a70a351f2e67f460a6f381c437e114
jebw <jeb@jdwilkins.co.uk>
2022-07-28 14:31:24 +0100

    HOTT-1789 Added specs for Rules met journeys

--- spec/features/rules_of_origin_wizard_spec.rb
+++ spec/features/rules_of_origin_wizard_spec.rb
@@ -80,6 +80,89 @@ RSpec.feature 'Rules of Origin wizard', type: :feature do
 
       expect(page).to have_css 'h1', text: 'Rules of Origin not met'
     end
+
+    scenario 'Importing - Not wholly obtained - Sufficient Processing - Rules met' do
+      visit commodity_path(commodity, country: 'JP', anchor: 'rules-of-origin')
+
+      expect(page).to have_css 'h2', text: 'Preferential rules of origin for trading with Japan'
+      click_on 'Check rules of origin'
+
+      expect(page).to have_css 'h1', text: /Are you importing goods.*UK.*Japan\?/
+      choose 'I am importing goods'
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /are classed as 'originating'/
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /How 'wholly obtained' is defined/
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /What components/
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /Are your goods wholly obtained/
+      choose 'No, my goods are not wholly obtained'
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: 'Your goods are not wholly obtained'
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: 'Including parts or components'
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /non-originating parts been sufficiently processed/
+      choose 'Yes'
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: 'Do your goods meet the product-specific rules?'
+      choose schemes.first.rule_sets.first.rules.first.rule
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /Origin requirements met/
+      click_on 'See valid proofs of origin'
+
+      expect(page).to have_css 'h1', text: 'Valid proofs of origin'
+    end
+
+    scenario 'Importing - Not wholly obtained - Sufficient Processing - Rules not met' do
+      visit commodity_path(commodity, country: 'JP', anchor: 'rules-of-origin')
+
+      expect(page).to have_css 'h2', text: 'Preferential rules of origin for trading with Japan'
+      click_on 'Check rules of origin'
+
+      expect(page).to have_css 'h1', text: /Are you importing goods.*UK.*Japan\?/
+      choose 'I am importing goods'
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /are classed as 'originating'/
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /How 'wholly obtained' is defined/
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /What components/
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /Are your goods wholly obtained/
+      choose 'No, my goods are not wholly obtained'
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: 'Your goods are not wholly obtained'
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: 'Including parts or components'
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /non-originating parts been sufficiently processed/
+      choose 'Yes'
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: 'Do your goods meet the product-specific rules?'
+      choose 'goods do not meet any'
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: 'Rules of Origin not met'
+    end
   end
 
   context 'with single GSP trade agreement' do
@@ -180,6 +263,57 @@ RSpec.feature 'Rules of Origin wizard', type: :feature do
 
       expect(page).to have_css 'h1', text: /Are your goods wholly obtained/
       choose 'Yes, my goods are wholly obtained'
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /Origin requirements met/
+      click_on 'See valid proofs of origin'
+
+      expect(page).to have_css 'h1', text: 'Valid proofs of origin'
+    end
+  end
+
+  context 'with subdivided rules' do
+    let(:schemes) { build_list :rules_of_origin_scheme, 1, :subdivided }
+
+    scenario 'Importing - Not wholly obtained - Sufficient Processing - Subdivided - Rules met' do
+      visit commodity_path(commodity, country: 'JP', anchor: 'rules-of-origin')
+
+      expect(page).to have_css 'h2', text: 'Preferential rules of origin for trading with Japan'
+      click_on 'Check rules of origin'
+
+      expect(page).to have_css 'h1', text: /Are you importing goods.*UK.*Japan\?/
+      choose 'I am importing goods'
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /are classed as 'originating'/
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /How 'wholly obtained' is defined/
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /What components/
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /Are your goods wholly obtained/
+      choose 'No, my goods are not wholly obtained'
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: 'Your goods are not wholly obtained'
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: 'Including parts or components'
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /non-originating parts been sufficiently processed/
+      choose 'Yes'
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: 'Provide more information about your product'
+      choose schemes.first.rule_sets.first.subdivision
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: 'Do your goods meet the product-specific rules?'
+      choose schemes.first.rule_sets.first.rules.first.rule
       click_on 'Continue'
 
       expect(page).to have_css 'h1', text: /Origin requirements met/
