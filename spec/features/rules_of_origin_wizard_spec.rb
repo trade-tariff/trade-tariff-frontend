266b6f42777119fdb61e2a7440bb200be6536174
jebw <jeb@jdwilkins.co.uk>
2022-07-28 14:36:54 +0100

    HOTT-1789 Specs for single wholly obtained rule

--- spec/features/rules_of_origin_wizard_spec.rb
+++ spec/features/rules_of_origin_wizard_spec.rb
@@ -322,4 +322,65 @@ RSpec.feature 'Rules of Origin wizard', type: :feature do
       expect(page).to have_css 'h1', text: 'Valid proofs of origin'
     end
   end
+
+  context 'with single wholly obtained rule' do
+    let :schemes do
+      build_list :rules_of_origin_scheme, 1, :single_wholly_obtained_rule
+    end
+
+    scenario 'Importing - Wholly obtained' do
+      visit commodity_path(commodity, country: 'JP', anchor: 'rules-of-origin')
+
+      expect(page).to have_css 'h2', text: 'Preferential rules of origin for trading with Japan'
+      click_on 'Check rules of origin'
+
+      expect(page).to have_css 'h1', text: /Are you importing goods.*UK.*Japan\?/
+      choose 'I am importing goods'
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /are classed as 'originating'/
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /How 'wholly obtained' is defined/
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /What components/
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /Are your goods wholly obtained/
+      choose 'Yes, my goods are wholly obtained'
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /Origin requirements met/
+      click_on 'See valid proofs of origin'
+
+      expect(page).to have_css 'h1', text: 'Valid proofs of origin'
+    end
+
+    scenario 'Importing - Not wholly obtained' do
+      visit commodity_path(commodity, country: 'JP', anchor: 'rules-of-origin')
+
+      expect(page).to have_css 'h2', text: 'Preferential rules of origin for trading with Japan'
+      click_on 'Check rules of origin'
+
+      expect(page).to have_css 'h1', text: /Are you importing goods.*UK.*Japan\?/
+      choose 'I am importing goods'
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /are classed as 'originating'/
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /How 'wholly obtained' is defined/
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /What components/
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: /Are your goods wholly obtained/
+      choose 'No, my goods are not wholly obtained'
+      click_on 'Continue'
+
+      expect(page).to have_css 'h1', text: 'Rules of Origin not met'
+    end
+  end
 end
