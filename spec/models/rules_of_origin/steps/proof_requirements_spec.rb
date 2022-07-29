4e91e8c20a9ebe6060e6a6fd76f4e8352addba0f
jebw <jeb@jdwilkins.co.uk>
2022-07-29 12:26:24 +0100

    HOTT-1785 Changed back on final req met screens
    They always go back to Origin Requirements Met, even if navigated via
    sibling final screens

--- spec/models/rules_of_origin/steps/proof_requirements_spec.rb
+++ spec/models/rules_of_origin/steps/proof_requirements_spec.rb
@@ -6,22 +6,10 @@ RSpec.describe RulesOfOrigin::Steps::ProofRequirements do
 
   it_behaves_like 'an article accessor', :processes_text, 'origin_processes'
 
-  describe '#skipped' do
+  describe '#skipped?' do
     subject { instance.skipped? }
 
-    it { is_expected.to be false }
-
-    context "when 'wholly_obtained' set to 'yes'" do
-      include_context 'with rules of origin store', :wholly_obtained
-
-      it { is_expected.to be false }
-    end
-
-    context "when 'wholly_obtained' set to 'no'" do
-      include_context 'with rules of origin store', :not_wholly_obtained
-
-      it { is_expected.to be true }
-    end
+    it { is_expected.to be true }
   end
 
   describe 'processes article content' do
--- spec/models/rules_of_origin/steps/proof_verification_spec.rb
+++ spec/models/rules_of_origin/steps/proof_verification_spec.rb
@@ -4,22 +4,10 @@ RSpec.describe RulesOfOrigin::Steps::ProofVerification do
   include_context 'with rules of origin store', :originating
   include_context 'with wizard step', RulesOfOrigin::Wizard
 
-  describe '#skipped' do
+  describe '#skipped?' do
     subject { instance.skipped? }
 
-    it { is_expected.to be false }
-
-    context "when 'wholly_obtained' set to 'yes'" do
-      include_context 'with rules of origin store', :wholly_obtained
-
-      it { is_expected.to be false }
-    end
-
-    context "when 'wholly_obtained' set to 'no'" do
-      include_context 'with rules of origin store', :not_wholly_obtained
-
-      it { is_expected.to be true }
-    end
+    it { is_expected.to be true }
   end
 
   it_behaves_like 'an article accessor', :verification_text, 'verification'
