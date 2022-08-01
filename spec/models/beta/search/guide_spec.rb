823ba792718360cee957840a799f47de663af2e3
William Fish <william.michael.fish@gmail.com>
2022-08-01 11:28:30 +0100

    HOTT-1776: Code review

--- spec/models/beta/search/guide_spec.rb
+++ spec/models/beta/search/guide_spec.rb
@@ -3,8 +3,9 @@ require 'spec_helper'
 RSpec.describe Beta::Search::Guide do
   subject(:guide) { build(:guide) }
 
-  it { is_expected.to have_attributes(title: 'Herbal medicines',
-                                      url: 'Get help to classify herbal medicines',
-                                      strapline: 'This is the guide for Herbal medicines')
+  it {
+    expect(guide).to have_attributes(title: 'Herbal medicines',
+                                     url: 'Get help to classify herbal medicines',
+                                     strapline: 'This is the guide for Herbal medicines')
   }
 end
--- spec/models/beta/search/search_query_parser_result_spec.rb
+++ spec/models/beta/search/search_query_parser_result_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe Beta::Search::SearchQueryParserResult do
   subject(:search_query_parser_result) { build(:search_query_parser_result) }
 
   it {
-    expect(subject).to have_attributes(corrected_search_query: 'clothing set',
-                                       original_search_query: 'cloting set',
-                                       verbs: [],
-                                       adjectives: [],
-                                       nouns: ['clothing set'],
-                                       noun_chunks: ['clothing set'])
+    expect(search_query_parser_result).to have_attributes(corrected_search_query: 'clothing set',
+                                                          original_search_query: 'cloting set',
+                                                          verbs: [],
+                                                          adjectives: [],
+                                                          nouns: ['clothing set'],
+                                                          noun_chunks: ['clothing set'])
   }
 end
