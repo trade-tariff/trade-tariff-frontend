543afab634ace0f7458b279d7617562a9afabac6
William Fish <william.michael.fish@gmail.com>
2022-08-05 15:31:54 +0100

    HOTT-1793: Adds filter convenience methods

--- spec/models/beta/search/facet_filter_statistic_spec.rb
+++ spec/models/beta/search/facet_filter_statistic_spec.rb
@@ -11,4 +11,24 @@ RSpec.describe Beta::Search::FacetFilterStatistic do
                                                       display_name: 'Material',
                                                       question: 'What material?')
   }
+
+  describe '#facet' do
+    subject(:facet) { build(:facet_filter_statistic, facet_filter: 'filter_material').facet }
+
+    it { is_expected.to eq('material') }
+  end
+
+  describe '#find_classification' do
+    context 'when there is a matching classification' do
+      subject(:find_classification) { build(:facet_filter_statistic).find_classification('leather') }
+
+      it { is_expected.to be_a(Beta::Search::FacetClassificationStatistic) }
+    end
+
+    context 'when there is no matching classification' do
+      subject(:find_classification) { build(:facet_filter_statistic).find_classification('sausage') }
+
+      it { is_expected.to be_nil }
+    end
+  end
 end
