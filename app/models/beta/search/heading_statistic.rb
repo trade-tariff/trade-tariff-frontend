ceb08b8176da26bae13b35c2568f25823c896e19
William Fish <william.michael.fish@gmail.com>
2022-08-05 15:32:23 +0100

    HOTT-1793: Adds heading to heading_statistic

--- app/models/beta/search/heading_statistic.rb
+++ app/models/beta/search/heading_statistic.rb
@@ -11,6 +11,12 @@ module Beta
                     :chapter_id,
                     :chapter_description,
                     :chapter_score
+
+      def heading
+        goods_nomenclature = Heading.new
+        goods_nomenclature.goods_nomenclature_item_id = "#{resource_id}000000"
+        goods_nomenclature
+      end
     end
   end
 end
