dfae5aa89e34720db0be96598a7ee46b2595de54
William Fish <william.michael.fish@gmail.com>
2022-08-31 09:33:57 +0100

    Refactor pages controller
    - Removes unroutable pages index endpoint
    - Removes unroutable pages index view
    - Be explicit in controller about pages endpoints
    - Removes duplicate setter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,22 +2,13 @@ class PagesController < ApplicationController
   before_action :disable_search_form,
                 :disable_last_updated_footnote
 
-  def index
-    @section_css = 'govuk-visually-hidden'
-    @meta_description = I18n.t('meta_description')
-  end
-
   def cn2021_cn2022
     @chapters = Chapter.all
   end
 
-  def opensearch
-    respond_to do |format|
-      format.xml
-    end
-  end
-
-  def tools
-    disable_search_form
-  end
+  def terms; end
+  def tools; end
+  def privacy; end
+  def help; end
+  def help_find_commodity; end
 end
