e4b49cc67157fc9438a8a0ed3ad012bdfc7b3023
jebw <jeb@jdwilkins.co.uk>
2022-09-05 11:33:26 +0100

    HOTT-1937 Moved CSS to generic component
    Allows for future reuse

--- spec/views/rules_of_origin/steps/_components_definition.html.erb_spec.rb
+++ spec/views/rules_of_origin/steps/_components_definition.html.erb_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe 'rules_of_origin/steps/_components_definition', type: :view do
   it { is_expected.to have_css 'h3', text: /Next step/ }
   it { is_expected.to have_css 'p', text: %r{Click on the 'Continue' button.*#{schemes.first.title}} }
   it { is_expected.to have_css 'form button[type=submit]' }
-  it { is_expected.not_to have_css '.rules-of-origin-attachment-text' }
+  it { is_expected.not_to have_css '.downloadable-document__text' }
 
   context 'without accesories article' do
     let(:articles) { [] }
@@ -48,14 +48,14 @@ RSpec.describe 'rules_of_origin/steps/_components_definition', type: :view do
       ]
     end
 
-    it { is_expected.to have_css '.rules-of-origin-attachment-text', text: article_reference }
-    it { is_expected.to have_css '.rules-of-origin-attachment-text', text: schemes.first.origin_reference_document.ord_title }
+    it { is_expected.to have_css '.downloadable-document__text', text: article_reference }
+    it { is_expected.to have_css '.downloadable-document__text', text: schemes.first.origin_reference_document.ord_title }
     it { is_expected.to have_css 'a[href]', text: /Download rules of origin reference document/ }
     it { is_expected.to have_css '.subtext', text: schemes.first.origin_reference_document.ord_version }
     it { is_expected.to have_css '.subtext', text: schemes.first.origin_reference_document.ord_date }
 
-    it { is_expected.to have_css '.rules-of-origin-attachment-text', text: article_reference_2 }
+    it { is_expected.to have_css '.downloadable-document__text', text: article_reference_2 }
 
-    it { is_expected.to have_css '.rules-of-origin-attachment-text', text: article_reference_3 }
+    it { is_expected.to have_css '.downloadable-document__text', text: article_reference_3 }
   end
 end
