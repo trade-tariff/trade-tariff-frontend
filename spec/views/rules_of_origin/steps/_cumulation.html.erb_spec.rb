762434ae27f5690a349199d9eb25313b014eb819
Ata Dahri <ata-ul-rouf.dahri@transformuk.com>
2022-09-08 10:42:13 +0100

    HOTT-1873: Removed hardcoded cumulation methods

--- spec/views/rules_of_origin/steps/_cumulation.html.erb_spec.rb
+++ spec/views/rules_of_origin/steps/_cumulation.html.erb_spec.rb
@@ -11,9 +11,6 @@ RSpec.describe 'rules_of_origin/steps/_cumulation', type: :view, vcr: { cassette
   it { is_expected.to have_css 'h3.govuk-heading-s', text: %r{Cumulation.*#{schemes.first.title}} }
   it { is_expected.to have_css 'p', text: %r{Map showing countries.*#{schemes.first.title}} }
   it { is_expected.to have_css '.govuk-inset-text', text: %r{#{schemes.first.title}} }
-  it { is_expected.to have_css 'p strong', text: 'Bilateral cumulation' }
-  it { is_expected.to have_css 'figure img[src*="roo_wizard/bilateral"]' }
-  it { is_expected.to have_css 'figure figcaption' }
   it { is_expected.to have_css 'details.govuk-details summary', text: %r{cumulation.*#{schemes.first.title}} }
   it { is_expected.to have_css 'details.govuk-details div.govuk-details__text' }
   it { is_expected.to have_css 'h3.govuk-heading-s', text: /Next step/ }
@@ -22,6 +19,8 @@ RSpec.describe 'rules_of_origin/steps/_cumulation', type: :view, vcr: { cassette
   it { is_expected.to have_css 'p', text: %r{The #{schemes.first.title} .* 2 methods of cumulation} }
   it { is_expected.to have_css 'span', text: 'Bilateral cumulation - an example' }
   it { is_expected.to have_css 'span', text: 'Extended cumulation - an example' }
+  it { is_expected.to have_css 'p img[src*="cumulation/bilateral"]' }
+  it { is_expected.to have_css 'p img[src*="cumulation/extended"]' }
   it { is_expected.not_to have_css '.downloadable-document__text' }
 
   context 'with article reference' do
