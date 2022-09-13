d67272970f6c932c770ec1348a990c2f621ea551
William Fish <william.michael.fish@gmail.com>
2022-09-13 16:30:03 +0100

    HOTT-1617: Adds validity period specs WIP

--- spec/requests/subheadings_controller_spec.rb
+++ spec/requests/subheadings_controller_spec.rb
@@ -23,4 +23,43 @@ RSpec.describe SubheadingsController, type: :request do
 
   it { expect(page).to have_content 'Chapter notes' }
   it { expect(page).to have_content 'Section notes' }
+
+  # 404 -> validity periods
+  # 404 -> validity periods 404
+  context 'when the subheading does not exist and we fetch validity periods' do
+    before do
+      # http://localhost:3018/subheadings/0101210000-10?as_of=2022-09-13
+      stub_api_request("/subheadings/#{subheading_id}?as_of=2022-09-13")
+        .to_return(jsonapi_not_found_response)
+    end
+
+    let(:subheading_id) { '0101999999-80' } # subheading 0101999999 does not exist
+
+    context 'when the validity periods are present' do
+      before do
+        stub_api_request("/subheadings/#{subheading_id}/validity_periods")
+          .to_return jsonapi_response(:validity_periods, validity_periods)
+
+        visit subheading_path('0101210000-10')
+      end
+
+      let(:validity_periods) do
+        attributes_for_list :validity_period, 2,
+                            goods_nomenclature_item_id: subheading_id
+      end
+
+      it { is_expected.to render_template(:show_404) }
+    end
+
+    context 'when the validity periods returns not found' do
+      before do
+        stub_api_request("/subheadings/#{subheading_id}/validity_periods")
+          .to_return jsonapi_response(:validity_periods, validity_periods)
+
+        visit subheading_path('0101210000-10')
+      end
+
+      it { is_expected.to render_template(:show_404) }
+    end
+  end
 end
