5283f84de914fb76bae0692d2927bcc28a565e9c
William Fish <william.michael.fish@gmail.com>
2022-09-14 12:21:10 +0100

    HOTT-1617: Adds updated factories for validity periods

--- spec/factories/validity_period_factory.rb
+++ spec/factories/validity_period_factory.rb
@@ -12,12 +12,30 @@ FactoryBot.define do
       goods_nomenclature&.code || sprintf('0101%06d', n)
     end
 
+    id { 'foo'}
+
     validity_start_date { (months_ago || 1).months.ago }
 
     validity_end_date do
       if months_ago && months_ago > 1
         (months_ago - 1).months.ago - 1.day
       end
+    end
+
+    trait :subheading do
+      goods_nomenclature_item_id { '0101999999' }
+      producline_suffix { '80' }
+      goods_nomenclature_class { 'Subheading' }
+
+      to_param { "#{goods_nomenclature_item_id}-#{producline_suffix}" }
+    end
+
+    trait :commodity do
+      goods_nomenclature_item_id { '0101999999' }
+      producline_suffix { '80' }
+      goods_nomenclature_class { 'Commodity' }
+
+      to_param { goods_nomenclature_item_id }
     end
   end
 end
