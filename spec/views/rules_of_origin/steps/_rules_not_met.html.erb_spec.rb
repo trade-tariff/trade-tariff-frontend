d0552203d5d87c491ef8e59de132f45ed2ecd310
Ata Dahri <ata-ul-rouf.dahri@transformuk.com>
2022-09-15 15:13:47 +0100

    HOTT-1935: Fix country name

--- spec/views/rules_of_origin/steps/_rules_not_met.html.erb_spec.rb
+++ spec/views/rules_of_origin/steps/_rules_not_met.html.erb_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'rules_of_origin/steps/_rules_not_met', type: :view do
                   'rules_not_met',
                   :insufficient_processing
 
-  it { is_expected.to have_css 'span.govuk-caption-xl', text: %r{(Im|Ex)porting.* #{wizardstore['commodity_code']}.*Japan} }
+  it { is_expected.to have_css 'span.govuk-caption-xl', text: %r{(Im|Ex)porting.* #{wizardstore['commodity_code']}.*Japan.*UK} }
   it { is_expected.to have_css 'h1', text: /not met/i }
   it { is_expected.to have_css '.govuk-panel--confirmation .govuk-panel__body', count: 1 }
   it { is_expected.to have_css '.govuk-panel__body', text: %r{#{schemes.first.title}} }
