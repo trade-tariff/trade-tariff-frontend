16d1f2a23d9203cf3e0e87a8468db045a62cbd3d
jebw <jeb@jdwilkins.co.uk>
2022-09-20 10:42:06 +0100

    [DROP THIS COMMIT] Add temporary test mechanism for triggering exceptions

--- app/controllers/find_commodities_controller.rb
+++ app/controllers/find_commodities_controller.rb
@@ -2,6 +2,8 @@ class FindCommoditiesController < ApplicationController
   before_action :disable_switch_service_banner, only: [:show]
 
   def show
+    raise "An Error" if params[:trigger_exception] == 'trigger'
+
     @no_shared_search = true
     @latest_news = NewsItem.latest_for_home_page
   end
