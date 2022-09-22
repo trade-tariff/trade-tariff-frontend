f17a16689f9dbbd69d93bada25d7dbb427817357
William Fish <william.michael.fish@gmail.com>
2022-09-22 16:38:12 +0100

    HOTT-2023: Lint

--- spec/views/rules_of_origin/_tab.html.erb_spec.rb
+++ spec/views/rules_of_origin/_tab.html.erb_spec.rb
@@ -8,24 +8,14 @@ RSpec.describe 'rules_of_origin/_tab', type: :view do
            country_code: 'FR',
            country_name: 'France',
            commodity_code: '2203000100',
-           rules_of_origin_schemes: schemes,
-           declarable: build(:commodity, :with_import_trade_summary, preferential_tariff_duty:, preferential_quota_duty:)
+           rules_of_origin_schemes:,
+           declarable: build(:commodity, import_trade_summary:)
   end
 
-  let :rules_data do
-    attributes_for_list :rules_of_origin_rule,
-                        1,
-                        rule: "Manufacture\n\n* From materials"
-  end
-
-  let(:schemes) do
-    build_list :rules_of_origin_scheme, 1, rules: rules_data, fta_intro:
-  end
-
+  let(:rules) { attributes_for_list :rules_of_origin_rule, 1, rule: "Manufacture\n\n* From materials" }
+  let(:rules_of_origin_schemes) { [build(:rules_of_origin_scheme, rules:, fta_intro:)] }
   let(:fta_intro) { "## Free Trade Agreement\n\nDetails of agreement" }
-
-  let(:preferential_tariff_duty) { nil }
-  let(:preferential_quota_duty) { nil }
+  let(:import_trade_summary) { attributes_for(:import_trade_summary) }
 
   it 'includes the countries name in the title' do
     expect(rendered_page).to \
@@ -44,16 +34,12 @@ RSpec.describe 'rules_of_origin/_tab', type: :view do
     expect(rendered_page).to have_css '#rules-of-origin__intro--bloc-scheme'
   end
 
-  context 'with matched rules of origin' do
-    let(:first_rule) { schemes[0].rules[0] }
-
-    it 'includes the non-preferential bloc' do
-      expect(rendered_page).to have_css '.rules-of-origin__non-preferential'
-    end
+  it 'includes the non-preferential bloc' do
+    expect(rendered_page).to have_css '.rules-of-origin__non-preferential'
   end
 
   context 'without matched rules' do
-    let(:rules_data) { [] }
+    let(:rules) { [] }
 
     it 'does not shows rules table' do
       expect(rendered_page).not_to have_css 'table.govuk-table'
@@ -65,15 +51,13 @@ RSpec.describe 'rules_of_origin/_tab', type: :view do
   end
 
   context 'with country specific scheme' do
-    let(:schemes) do
-      build_list :rules_of_origin_scheme, 1, rules: rules_data, countries: %w[FR]
-    end
+    let(:rules_of_origin_schemes) { build_list :rules_of_origin_scheme, 1, rules:, countries: %w[FR] }
 
     it { is_expected.to have_css '#rules-of-origin__intro--country-scheme' }
   end
 
   context 'with no scheme' do
-    let(:schemes) { [] }
+    let(:rules_of_origin_schemes) { [] }
 
     it { is_expected.to have_css '#rules-of-origin__intro--no-scheme' }
     it { is_expected.not_to have_css '.rules-of-origin__scheme' }
@@ -84,8 +68,8 @@ RSpec.describe 'rules_of_origin/_tab', type: :view do
   end
 
   context 'with multiple schemes' do
-    let(:schemes) do
-      build_list :rules_of_origin_scheme, 3, rules: rules_data,
+    let(:rules_of_origin_schemes) do
+      build_list :rules_of_origin_scheme, 3, rules:,
                                              fta_intro:
     end
 
@@ -104,18 +88,20 @@ RSpec.describe 'rules_of_origin/_tab', type: :view do
 
   describe 'import trade summary duty box' do
     context 'when preferential tariff duty is present' do
-      let(:preferential_tariff_duty) { '1.00 %' }
+      let(:import_trade_summary) { attributes_for(:import_trade_summary, preferential_tariff_duty: '1.00 %') }
 
       it { expect(rendered_page).to have_css '.preferential-tariff-duty' }
     end
 
     context 'when preferential quota duty is present' do
-      let(:preferential_quota_duty) { '1.00 %' }
+      let(:import_trade_summary) { attributes_for(:import_trade_summary, preferential_quota_duty: '1.00 %') }
 
       it { expect(rendered_page).to have_css '.preferential-quota-duty' }
     end
 
     context 'when both preferential duties are missing' do
+      let(:import_trade_summary) { attributes_for(:import_trade_summary) }
+
       it { expect(rendered_page).not_to have_css '.preferential-tariff-duty' }
       it { expect(rendered_page).not_to have_css '.preferential-quota-duty' }
 
