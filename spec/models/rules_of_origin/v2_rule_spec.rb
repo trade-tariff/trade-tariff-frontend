e591f577c5317ed034e8a13227a24f099ee62d6e
jebw <jeb@jdwilkins.co.uk>
2022-10-11 15:00:57 +0100

    HOTT-2125 Updated branch logic for wholly obtained
    The Rules of Origin wizard will not give the user the option to choose a
    rule if they have stated their trade was not wholly obtained, and all
    applicable rules require the trade to have been wholly obtained.

--- spec/models/rules_of_origin/v2_rule_spec.rb
+++ spec/models/rules_of_origin/v2_rule_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe RulesOfOrigin::V2Rule do
   it { is_expected.to respond_to :rule_class }
   it { is_expected.to respond_to :operator }
 
-  describe '#wholly_obtained_class?' do
-    subject { described_class.new(rule_class:).wholly_obtained_class? }
+  describe '#only_wholly_obtained_class?' do
+    subject { described_class.new(rule_class:).only_wholly_obtained_class? }
 
     context 'with no classes' do
       let(:rule_class) { nil }
@@ -20,8 +20,14 @@ RSpec.describe RulesOfOrigin::V2Rule do
       it { is_expected.to be false }
     end
 
-    context 'with wholly obtained class' do
+    context 'with multiple classes including wholly obtained' do
       let(:rule_class) { %w[A1 WO B2] }
+
+      it { is_expected.to be false }
+    end
+
+    context 'with only wholly obtained class' do
+      let(:rule_class) { %w[WO] }
 
       it { is_expected.to be true }
     end
