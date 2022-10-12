14be5a2722e235aba61b5ecfb1f1efab67eed490
Alexdesi <alexdesi@gmail.com>
2022-10-12 15:35:24 +0100

    HOTT-2102: Rubocop lint fixes.

--- app/helpers/webchat_helper.rb
+++ app/helpers/webchat_helper.rb
@@ -1,6 +1,6 @@
 module WebchatHelper
   def webchat_link
-    link_to('Digital Assistant (opens in new tab)', "#{TradeTariffFrontend.webchat_url}", target: '_blank', rel: 'noopener')
+    link_to('Digital Assistant (opens in new tab)', TradeTariffFrontend.webchat_url, target: '_blank', rel: 'noopener')
   end
 
   def webchat_visible_in_footer?
--- spec/helpers/webchat_helper_spec.rb
+++ spec/helpers/webchat_helper_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe WebchatHelper, type: :helper do
   describe '#webchat_link' do
     it { expect(webchat_link).to include(ENV['WEBCHAT_URL']) }
   end
-  
+
   describe '#webchat_visible_in_footer?' do
     context('when the controller supports webchat link') do
       let(:controller_name) { %w[commodities headings subheadings chapters sections].sample }
