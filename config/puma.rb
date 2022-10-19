efb6fc3796561ea78dc356faff479154ded5d22c
William Fish <william.michael.fish@gmail.com>
2022-10-19 11:40:23 +0100

    Removes dead puma config

--- config/puma.rb
+++ config/puma.rb
@@ -6,15 +6,3 @@ preload_app!
 
 port        ENV['PORT']     || 3001
 environment ENV['RACK_ENV'] || 'development'
-
-on_worker_boot do
-  # Ensure we don't keep connections
-  if defined?(Sequel)
-    ::Sequel::Model.db.disconnect
-    ::Sequel::DATABASES.each(&:disconnect)
-  end
-end
-
-after_worker_boot do
-  SequelRails.setup Rails.env if defined?(SequelRails)
-end
