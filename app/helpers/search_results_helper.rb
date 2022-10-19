c8a7a240eb9d312f4d9bc7cc657191f0cadfed3b
Alexdesi <alexdesi@gmail.com>
2022-10-19 15:23:34 +0100

    HOTT-2147 Add ancestor links withing breadcrumbs to improve navigability.

--- app/helpers/search_results_helper.rb
+++ app/helpers/search_results_helper.rb
@@ -64,4 +64,10 @@ module SearchResultsHelper
     options.delete(:country)
     options
   end
+
+  def ancestor_links(hit, css_class: nil)
+    hit.ancestors[1..].map do |good_nomenclature|
+      link_to(good_nomenclature.description, polymorphic_path(good_nomenclature), class: css_class)
+    end
+  end
 end
