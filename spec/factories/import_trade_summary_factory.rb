2fe9d99c474c28dfe1dc4d1c4b236f635fd4ccf1
jebw <jeb@jdwilkins.co.uk>
2022-10-24 12:26:35 +0100

    HOTT-2124 Don't show contradictory info on RoO tab

--- spec/factories/import_trade_summary_factory.rb
+++ spec/factories/import_trade_summary_factory.rb
@@ -3,5 +3,13 @@ FactoryBot.define do
     basic_third_country_duty { '<span>2.00</span>' }
     preferential_tariff_duty { nil }
     preferential_quota_duty  { nil }
+
+    trait :with_tariff_duty do
+      preferential_tariff_duty { '1.00 %' }
+    end
+
+    trait :with_quota_duty do
+      preferential_quota_duty { '1.00 %' }
+    end
   end
 end
--- spec/views/rules_of_origin/_tab.html.erb_spec.rb
+++ spec/views/rules_of_origin/_tab.html.erb_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe 'rules_of_origin/_tab', type: :view do
   let(:rules) { attributes_for_list :rules_of_origin_rule, 1, rule: "Manufacture\n\n* From materials" }
   let(:rules_of_origin_schemes) { [build(:rules_of_origin_scheme, rules:, fta_intro:)] }
   let(:fta_intro) { "## Free Trade Agreement\n\nDetails of agreement" }
-  let(:import_trade_summary) { attributes_for(:import_trade_summary) }
+  let(:import_trade_summary) { attributes_for(:import_trade_summary, :with_tariff_duty) }
 
   it 'includes the countries name in the title' do
     expect(rendered_page).to \
@@ -88,13 +88,13 @@ RSpec.describe 'rules_of_origin/_tab', type: :view do
 
   describe 'import trade summary duty box' do
     context 'when preferential tariff duty is present' do
-      let(:import_trade_summary) { attributes_for(:import_trade_summary, preferential_tariff_duty: '1.00 %') }
+      let(:import_trade_summary) { attributes_for(:import_trade_summary, :with_tariff_duty) }
 
       it { expect(rendered_page).to have_css '.preferential-tariff-duty' }
     end
 
     context 'when preferential quota duty is present' do
-      let(:import_trade_summary) { attributes_for(:import_trade_summary, preferential_quota_duty: '1.00 %') }
+      let(:import_trade_summary) { attributes_for(:import_trade_summary, :with_quota_duty) }
 
       it { expect(rendered_page).to have_css '.preferential-quota-duty' }
     end
