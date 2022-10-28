3e84bc3003b3b6d9daa0f1ae8d08ed8b878b083a
jebw <jeb@jdwilkins.co.uk>
2022-10-28 15:23:40 +0100

    HOTT-2175 Added Origin reference logic to articles

--- app/models/rules_of_origin/article.rb
+++ app/models/rules_of_origin/article.rb
@@ -4,4 +4,16 @@ class RulesOfOrigin::Article
   include ApiEntity
 
   attr_accessor :article, :content
+
+  def text
+    content.sub(/{{.*}}/i, '') if content
+  end
+
+  def ord_reference
+    content.scan(/{{(.*)}}/i).first&.first&.strip if content
+  end
+
+  def ord_reference?
+    !ord_reference.nil?
+  end
 end
