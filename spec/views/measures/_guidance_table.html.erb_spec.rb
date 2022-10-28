4c344cb0ea5ac36302cfb4297cc6cc62684c88d7
William Fish <william.michael.fish@gmail.com>
2022-10-28 15:36:27 +0100

    HOTT-2178: Code review

--- spec/views/measures/_guidance_table.html.erb_spec.rb
+++ spec/views/measures/_guidance_table.html.erb_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 RSpec.describe 'measures/guidance_table', type: :view do
   subject { render_page && rendered }
 
-  let(:render_page) { render 'measures/guidance_table', measure_conditions_with_guidance:, anchor: }
+  let(:render_page) { render 'measures/guidance_table', measure_conditions_with_guidance:, include_chief_guidance: }
 
-  context 'when the page anchor is `export`' do
+  context 'when the page include_chief_guidance is `true`' do
     let(:measure_conditions_with_guidance) { build_list :measure_condition, 2, :with_guidance }
-    let(:anchor) { 'export' }
+    let(:include_chief_guidance) { true }
 
     it { is_expected.to have_css 'details summary', text: /Guidance for completing/ }
     it { is_expected.to have_css 'table tbody tr', count: 2 }
@@ -22,9 +22,9 @@ RSpec.describe 'measures/guidance_table', type: :view do
     end
   end
 
-  context 'when the page anchor is `import`' do
+  context 'when the page include_chief_guidance is `import`' do
     let(:measure_conditions_with_guidance) { build_list :measure_condition, 2, :with_guidance }
-    let(:anchor) { 'import' }
+    let(:include_chief_guidance) { false }
 
     it { is_expected.to have_css 'details summary', text: /Guidance for completing/ }
     it { is_expected.to have_css 'table tbody tr', count: 2 }
