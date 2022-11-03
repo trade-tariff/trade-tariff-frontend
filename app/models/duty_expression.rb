0b65d23b9064cc4d001022db1f4d68e319b2c2a6
Ata Dahri <ata-ul-rouf.dahri@transformuk.com>
2022-11-03 14:28:01 +0000

    HOTT-2071: Use verbose duty field on commodities and headings pages only

--- app/models/duty_expression.rb
+++ app/models/duty_expression.rb
@@ -3,11 +3,11 @@ require 'api_entity'
 class DutyExpression
   include ApiEntity
 
-  attr_accessor :base, :formatted_base
+  attr_accessor :base, :formatted_base, :verbose_duty
 
   def to_s
-    if formatted_base.present?
-      formatted_base
+    if verbose_duty.present?
+      verbose_duty
     else
       base
     end
