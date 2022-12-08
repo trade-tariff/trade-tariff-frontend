eddac99181f8949ef60499a8a1ac272677f6d2c0
jebw <jeb@jdwilkins.co.uk>
2022-12-08 10:49:35 +0000

    HOTT-2261 Add GovUK Pub Components to webpacker path

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,5 @@
 const { environment } = require('@rails/webpacker');
+const { execSync } = require('child_process')
 const webpack = require('webpack');
 
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
@@ -6,5 +7,20 @@ environment.plugins.append('Provide', new webpack.ProvidePlugin({
   jQuery: 'jquery',
   BetaPopup: 'popup'
 }));
+
+let gpcGemPath = null
+try {
+  gpcGemPath = execSync('bundle show govuk_publishing_components')
+    .toString()
+    .replace(/\n$/, '')
+} catch (err) {
+  console.error("Expected govuk_publishing_components gem to be installed")
+  process.exit()
+}
+
+const config = environment.toWebpackConfig()
+config.resolve.modules.push(`${gpcGemPath}/app/assets/images/govuk_publishing_components`)
+config.resolve.modules.push(`${gpcGemPath}/app/assets/stylesheets/govuk_publishing_components`)
+config.resolve.modules.push(`${gpcGemPath}/app/assets/javascripts/govuk_publishing_components`)
 
 module.exports = environment;
