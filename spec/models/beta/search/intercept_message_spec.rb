3f2849da81bcd913fc99cffd547604193ec89c97
jebw <jeb@jdwilkins.co.uk>
2023-01-17 10:24:44 +0000

    HOTT-2471 Autolink intercept messages

--- spec/models/beta/search/intercept_message_spec.rb
+++ spec/models/beta/search/intercept_message_spec.rb
@@ -1,14 +1,22 @@
 require 'spec_helper'
 
 RSpec.describe Beta::Search::InterceptMessage do
-  describe '#html_message' do
-    subject(:intercept_message) do
-      formatted_message = '[chapter 85](/chapters/85)'
+  subject(:intercept_message) { build :intercept_message, formatted_message: }
 
-      build(:intercept_message, formatted_message:).html_message
-    end
+  let(:formatted_message) { '[chapter 85](/chapters/85)' }
+
+  describe '#html_message' do
+    subject { intercept_message.html_message }
 
     it { is_expected.to be_html_safe }
     it { is_expected.to include('<a href="/chapters/85">chapter 85</a>') }
+
+    context 'with unmarkuped hyperlink' do
+      let(:formatted_message) { 'this is a *link* to https://www.gov.uk/' }
+
+      it { is_expected.to be_html_safe }
+      it { is_expected.to include '<em>link</em>' }
+      it { is_expected.to include '<a href="https://www.gov.uk/">https://www.gov.uk/</a>' }
+    end
   end
 end
