d7352966255243fa2d839e1b8f240e1f9cdff400
William Fish <william.michael.fish@gmail.com>
2023-02-08 16:29:23 +0000

    HOTT-2622: Adds UI for deriving goods nomenclatures

--- app/models/validity_period.rb
+++ app/models/validity_period.rb
@@ -6,8 +6,12 @@ class ValidityPeriod
   attr_accessor :goods_nomenclature_item_id,
                 :validity_start_date,
                 :validity_end_date,
+                :description,
+                :formatted_description,
                 :to_param,
                 :goods_nomenclature_class
+
+  has_many :deriving_goods_nomenclatures, polymorphic: true
 
   def start_date
     validity_start_date&.to_date
