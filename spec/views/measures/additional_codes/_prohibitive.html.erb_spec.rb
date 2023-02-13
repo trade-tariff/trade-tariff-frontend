5f9705efb81c43afc1b7be4112a954822a905226
William Fish <william.michael.fish@gmail.com>
2023-02-13 13:23:27 +0000

    HOTT-2592: Lint

--- spec/views/measures/additional_codes/_prohibitive.html.erb_spec.rb
+++ spec/views/measures/additional_codes/_prohibitive.html.erb_spec.rb
@@ -9,23 +9,23 @@ RSpec.describe 'measures/additional_codes/_prohibitive', type: :view do
     render 'measures/additional_codes/prohibitive', measure: MeasurePresenter.new(measure)
   end
 
-    context 'when residual returns true' do
-      let(:measure) { build(:measure, :residual) }
+  context 'when residual returns true' do
+    let(:measure) { build(:measure, :residual) }
 
-      it { expect(rendered).to match(/Control does not apply to goods/) }
+    it { expect(rendered).to match(/Control does not apply to goods/) }
 
-      it { expect(rendered).to match(measure.additional_code.code) }
+    it { expect(rendered).to match(measure.additional_code.code) }
 
-      it { expect(rendered).to match(measure.additional_code.formatted_description) }
-    end
+    it { expect(rendered).to match(measure.additional_code.formatted_description) }
+  end
 
-    context 'when residual returns false' do
-      let(:measure) { build(:measure, :with_additional_code) }
+  context 'when residual returns false' do
+    let(:measure) { build(:measure, :with_additional_code) }
 
-      it { expect(rendered).to match(/Control applies to goods/) }
+    it { expect(rendered).to match(/Control applies to goods/) }
 
-      it { expect(rendered).to match(measure.additional_code.code) }
+    it { expect(rendered).to match(measure.additional_code.code) }
 
-      it { expect(rendered).to match(measure.additional_code.formatted_description) }
-    end
+    it { expect(rendered).to match(measure.additional_code.formatted_description) }
+  end
 end
