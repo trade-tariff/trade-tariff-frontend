6667f57b7ac290f55ecda4141803be23270672dd
William Fish <william.michael.fish@gmail.com>
2023-02-16 14:10:57 +0000

    HOTT-2652: Remove sentry alerts for missing guide images, too

--- app/helpers/image_guides_helper.rb
+++ app/helpers/image_guides_helper.rb
@@ -4,8 +4,6 @@ module ImageGuidesHelper
 
     image_pack_tag "guides/#{guide_filename}", **kwargs.merge(class: 'image-guide')
   rescue Webpacker::Manifest::MissingEntryError
-    Sentry.capture_message "Missing guide image file: #{guide_filename}"
-
     nil
   end
 end
--- spec/helpers/image_guides_helper_spec.rb
+++ spec/helpers/image_guides_helper_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper'
 
 RSpec.describe ImageGuidesHelper, type: :helper do
   describe '#image_for_guide' do
-    before { allow(Sentry).to receive(:capture_message).and_return(true) }
-
     context 'when image for the guides does exist' do
       subject(:rendered) { helper.image_for_guide('rice.png') }
 
@@ -22,15 +20,9 @@ RSpec.describe ImageGuidesHelper, type: :helper do
     end
 
     context 'when the image for the guide does NOT exist' do
-      subject(:rendered) { helper.country_flag_tag('non-existent-guide') }
+      subject(:rendered) { helper.image_for_guide('non-existent-guide') }
 
       it('returns nothing') { is_expected.to be_nil }
-
-      it 'notifies Sentry' do
-        rendered
-
-        expect(Sentry).to have_received(:capture_message)
-      end
     end
   end
 end
