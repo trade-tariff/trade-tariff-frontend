f83b3123a0507a05997caa483f59d53e8c26a177
William Fish <william.michael.fish@gmail.com>
2023-02-16 14:12:29 +0000

    HOTT-2652: Remove unused constant

--- app/helpers/country_flag_helper.rb
+++ app/helpers/country_flag_helper.rb
@@ -1,6 +1,4 @@
 module CountryFlagHelper
-  COUNTRY_FLAG_IGNORE = %w[xi xc xl].freeze
-
   def country_flag_tag(two_letter_country_code, **kwargs)
     two_letter_country_code = two_letter_country_code.downcase
 
