96ee0e23074d19c39a3b931d977c68959948c285
William Fish <william.michael.fish@gmail.com>
2023-02-16 13:52:56 +0000

    HOTT-2652: Silence sentry noise for country scrapers
    https://engine-le.sentry.io/issues/3929079904/?project=5557002&referrer=slack

--- app/helpers/country_flag_helper.rb
+++ app/helpers/country_flag_helper.rb
@@ -7,11 +7,6 @@ module CountryFlagHelper
     image_pack_tag "flags/#{two_letter_country_code}.png",
                    **kwargs.merge(class: 'country-flag')
   rescue Webpacker::Manifest::MissingEntryError
-    unless CountryFlagHelper::COUNTRY_FLAG_IGNORE.include?(two_letter_country_code)
-      Sentry.capture_message \
-        "Missing flag image file for #{two_letter_country_code}"
-    end
-
     nil
   end
 end
