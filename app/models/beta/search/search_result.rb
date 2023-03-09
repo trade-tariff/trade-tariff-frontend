59d986ab6d70ea90b3f4f443519c792230a785f2
William Fish <william.michael.fish@gmail.com>
2023-03-09 12:30:08 +0000

    HOTT-2774: Adds collection wrapper with default filter

--- app/models/beta/search/search_result.rb
+++ app/models/beta/search/search_result.rb
@@ -14,20 +14,28 @@ module Beta
       delegate :spelling_corrected?, :original_search_query, :corrected_search_query, to: :search_query_parser_result
 
       has_many :chapter_statistics, class_name: 'Beta::Search::ChapterStatistic'
-      has_many :heading_statistics, class_name: 'Beta::Search::HeadingStatistic', wrapper: Beta::Search::HeadingStatisticCollection
-      has_many :hits, polymorphic: true, wrapper: Beta::Search::HitsCollection
+
+      has_many :heading_statistics,
+               class_name: 'Beta::Search::HeadingStatistic',
+               wrapper: Beta::Search::HeadingStatisticCollection
+
+      has_many :facet_filter_statistics,
+               class_name: 'Beta::Search::FacetFilterStatistic',
+               wrapper: Beta::Search::FacetFilterStatisticCollection,
+               filter: :applicable
+
+      has_many :hits,
+               polymorphic: true,
+               wrapper: Beta::Search::HitsCollection
+
       has_one :direct_hit, polymorphic: true
-      has_many :facet_filter_statistics, class_name: 'Beta::Search::FacetFilterStatistic'
+
       has_one :guide, class_name: 'Beta::Search::Guide'
       has_one :search_query_parser_result, class_name: 'Beta::Search::SearchQueryParserResult'
       has_one :intercept_message, class_name: 'Beta::Search::InterceptMessage'
 
       def multiple_headings_view?
         heading_statistics.count > TradeTariffFrontend.beta_search_heading_statistics_threshold
-      end
-
-      def applicable_facet_filter_statistics
-        facet_filter_statistics.select { |facet_filter_statistic| facet_filter_statistic.facet_classification_statistics.many? }
       end
 
       def classification_for(facet, classification)
