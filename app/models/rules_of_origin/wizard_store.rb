4b321147eaccc5b1002474b091730f4c164d77cf
jebw <jeb@jdwilkins.co.uk>
2023-05-12 10:01:53 +0100

    HOTT-3166 Always GC wizard data
    Stop gap solutions for existing sessions which are too large to have
    additional data written to them

--- app/models/rules_of_origin/wizard_store.rb
+++ app/models/rules_of_origin/wizard_store.rb
@@ -3,6 +3,17 @@ module RulesOfOrigin
     PREFIX = 'rules_of_origin-'.freeze
     MAX_JOURNEYS = 5
 
+    class << self
+      def gc_store_data(backing_store)
+        backing_store.keys
+                     .select { |k| k.start_with? PREFIX }
+                     .sort_by { |k| backing_store[k]['modified'] || '2022-01-01T00:00:00Z' }
+                     .reverse
+                     .slice(MAX_JOURNEYS..)
+                     &.each { |k| backing_store.delete k }
+      end
+    end
+
     def initialize(backing_store, key)
       @backing_store = backing_store
       @backing_store["#{PREFIX}#{key}"] ||= {}
@@ -12,19 +23,8 @@ module RulesOfOrigin
 
     def persist(attributes)
       super(attributes.merge('modified' => Time.zone.now.iso8601)).tap do
-        gc_old_data
+        self.class.gc_store_data(@backing_store)
       end
-    end
-
-  private
-
-    def gc_old_data
-      @backing_store.keys
-                    .select { |k| k.start_with? PREFIX }
-                    .sort_by { |k| @backing_store[k]['modified'] || '2022-01-01T00:00:00Z' }
-                    .reverse
-                    .slice(MAX_JOURNEYS..)
-                    &.each { |k| @backing_store.delete k }
     end
   end
 end
