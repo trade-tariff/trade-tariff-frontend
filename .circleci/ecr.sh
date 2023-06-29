16292a6433ba41a114681461ae8f78ebb93bb50e
Amber Cronin <amber@amber.vision>
2023-06-29 14:16:35 +0100

    HOTT-3513: Add build job for new AWS environment

---
+++ .circleci/ecr.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+environment=$1
+docker_tag=$(git rev-parse --short HEAD)
+container="${IMAGE_NAME}:${docker_tag}"
+
+function fetch_ecr_url {
+  json=$(aws ssm get-parameter     \
+  --name "/${environment}/ECR_URL" \
+  --with-decryption                \
+  --output json                    \
+  --color off)
+
+  output=$(jq -r .Parameter.Value <<< "${json}")
+
+  if [ -n "${output}" ]; then
+    echo "${output}"
+  else
+    exit 1
+  fi
+}
+
+ecr_url=$(fetch_ecr_url)
+
+docker build -t "$container" .
+docker tag "$container" "${ecr_url}/${container}"
+
+aws ecr get-login-password --region "$AWS_DEFAULT_REGION" |
+  docker login --username AWS --password-stdin "$ecr_url"
+
+docker push "${ecr_url}/${container}"
