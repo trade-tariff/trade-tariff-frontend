e21068a307dedc293779011b5ea78214d86e80a7
Amber Cronin <amber@amber.vision>
2023-07-11 11:26:47 +0100

    HOTT-3514: Add IAM resource for new secret

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -9,7 +9,8 @@ data "aws_iam_policy_document" "secrets" {
     ]
     resources = [
       data.aws_secretsmanager_secret.redis_connection_string.arn,
-      data.aws_secretsmanager_secret.frontend_secret_key_base.arn
+      data.aws_secretsmanager_secret.frontend_secret_key_base.arn,
+      data.aws_secretsmanager_secret.newrelic_license_key.arn
     ]
   }
 
