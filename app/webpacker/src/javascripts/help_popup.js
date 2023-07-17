3cd0353bd20e992a1b2494b84002fc904c1e0ed2
William Fish <william.michael.fish@gmail.com>
2023-07-17 11:13:15 +0100

    BAU: Lint

--- app/webpacker/src/javascripts/help_popup.js
+++ app/webpacker/src/javascripts/help_popup.js
@@ -1,39 +1,39 @@
-$("#close_box").on("click", function(event) {
-  hide_help_popup();
-})
+$('#close_box').on('click', function(event) {
+  hideHelpPopup();
+});
 
-$("#close_box_longer").on("click", function(event) {
-  let expiresInDays = 28;
-  setCookie("show_digital_assistant_popup", "disable", expiresInDays);
-  hide_help_popup();
-})
+$('#close_box_longer').on('click', function(event) {
+  const expiresInDays = 28;
+  setCookie('show_digital_assistant_popup', 'disable', expiresInDays);
+  hideHelpPopup();
+});
 
-function hide_help_popup() {
-  $("#help_popup").css("display", "none");
+function hideHelpPopup() {
+  $('#help_popup').css('display', 'none');
 }
 
-setTimeout(show_help_popup, 30000);
+setTimeout(showHelpPopup, 30000);
 
-function show_help_popup() {
-  let cookieValue = getCookie("show_digital_assistant_popup");
-  let isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+function showHelpPopup() {
+  const cookieValue = getCookie('show_digital_assistant_popup');
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
-  if (cookieValue == "" && isMobile == false) {
-    $("#help_popup").fadeIn(1000);
+  if (cookieValue == '' && isMobile == false) {
+    $('#help_popup').fadeIn(1000);
   }
 }
 
 function setCookie(cname, cvalue, exdays) {
   const d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  let expires = "expires="+d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires;
+  const expires = 'expires='+d.toUTCString();
+  document.cookie = cname + '=' + cvalue + ';' + expires;
 }
 
 function getCookie(cname) {
-  let name = cname + "=";
-  let ca = document.cookie.split(';');
-  for(let i = 0; i < ca.length; i++) {
+  const name = cname + '=';
+  const ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
     let c = ca[i];
     while (c.charAt(0) == ' ') {
       c = c.substring(1);
@@ -42,5 +42,5 @@ function getCookie(cname) {
       return c.substring(name.length, c.length);
     }
   }
-  return "";
+  return '';
 }
