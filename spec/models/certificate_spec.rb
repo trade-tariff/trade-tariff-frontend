fcf972ef0ba24185e3ebcc3111abeced58e43126
William Fish <william.michael.fish@gmail.com>
2023-08-03 16:23:17 +0100

    HOTT-3408: Reflect change in certificate search

--- spec/models/certificate_spec.rb
+++ spec/models/certificate_spec.rb
@@ -2,10 +2,6 @@ require 'spec_helper'
 
 RSpec.describe Certificate do
   describe '.relationships' do
-    it { expect(described_class.relationships).to eq(%i[measures]) }
-  end
-
-  it_behaves_like 'an entity that has goods nomenclatures' do
-    let(:entity) { build(:certificate, measures:) }
+    it { expect(described_class.relationships).to eq(%i[goods_nomenclatures]) }
   end
 end
