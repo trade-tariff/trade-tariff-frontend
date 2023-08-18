4061b13df78d4f57dd8d8349e777e22b5d561c7b
William Fish <william.michael.fish@gmail.com>
2023-08-18 11:56:40 +0100

    HOTT-3611: Fix chrome api change

--- spec/models/rules_of_origin/scheme_spec.rb
+++ spec/models/rules_of_origin/scheme_spec.rb
@@ -421,13 +421,13 @@ RSpec.describe RulesOfOrigin::Scheme do
 
     let(:scheme) { build :rules_of_origin_scheme, countries: %w[FR] }
 
-    context 'when area in countries list' do
+    context 'when area in countries list and there is no show_proofs_for_geographical_areas list' do
       let(:area) { build :geographical_area, geographical_area_id: 'FR' }
 
       it { is_expected.to be true }
     end
 
-    context 'when area not in countries list' do
+    context 'when area not in countries list and there is no show_proofs_for_geographical_areas list' do
       let(:area) { build :geographical_area, geographical_area_id: 'DE' }
 
       it { is_expected.to be false }
@@ -438,13 +438,6 @@ RSpec.describe RulesOfOrigin::Scheme do
       let(:scheme) { build :rules_of_origin_scheme, show_proofs_for_geographical_areas: %w[AU] }
 
       it { is_expected.to be true }
-    end
-
-    context 'when show_proofs_for_geographical_areas is not specified' do
-      let(:area) { build :geographical_area, geographical_area_id: 'AU' }
-      let(:scheme) { build :rules_of_origin_scheme }
-
-      it { is_expected.to be false }
     end
   end
 
