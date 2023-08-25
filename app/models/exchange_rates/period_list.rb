21732e9f19dbde9ad376a2a34c963ea92be4e43d
William Fish <william.michael.fish@gmail.com>
2023-08-25 09:46:22 +0100

    BAU: Make period list api conform to common patterns

--- app/models/exchange_rates/period_list.rb
+++ app/models/exchange_rates/period_list.rb
@@ -8,11 +8,6 @@ class ExchangeRates::PeriodList
   has_many :exchange_rate_years, class_name: 'ExchangeRates::Year'
   has_many :exchange_rate_periods, class_name: 'ExchangeRates::Period'
 
-  def self.find(year, opts = {})
-    opts[:year] = year
-    super(nil, opts)
-  end
-
   def publication_date
     date_str = exchange_rate_periods.first.files.first&.publication_date
     date = Date.parse(date_str) if date_str
