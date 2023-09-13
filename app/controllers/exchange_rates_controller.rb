d02bba26a4335fbfde69a3bc494d7da0975952b3
Sam Johnson <sh.johnson@me.com>
2023-09-13 15:01:49 +0100

    fix content

--- app/controllers/exchange_rates_controller.rb
+++ app/controllers/exchange_rates_controller.rb
@@ -1,6 +1,6 @@
 class ExchangeRatesController < ApplicationController
-  before_action :disable_search_form,
-                :disable_switch_service_banner
+  before_action :disable_search_form, :disable_switch_service_banner
+  rescue_from Faraday::ResourceNotFound, with: :render_404
 
   def index
     @period_list = ExchangeRates::PeriodList.find(params[:year])
@@ -13,6 +13,10 @@ class ExchangeRatesController < ApplicationController
   end
 
   private
+
+  def render_404
+    render :show_404, status: :not_found
+  end
 
   def month
     id.split('-').last
--- app/models/exchange_rates/period_list.rb
+++ app/models/exchange_rates/period_list.rb
@@ -9,6 +9,8 @@ class ExchangeRates::PeriodList
   has_many :exchange_rate_periods, class_name: 'ExchangeRates::Period'
 
   def publication_date
+    raise Faraday::ResourceNotFound if exchange_rate_periods.empty?
+
     date_str = exchange_rate_periods.first.files.first&.publication_date
     date = Date.parse(date_str) if date_str
     date&.to_formatted_s(:long)
