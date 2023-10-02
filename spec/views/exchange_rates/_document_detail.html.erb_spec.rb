4c1217e26af3c980e709615808f1271d9f043850
Alexdesi <alexdesi@gmail.com>
2023-10-03 00:22:48 +0100

    HOTT-3976: Small refactor and fix spec for _document_detail.

--- spec/views/exchange_rates/_document_detail.html.erb_spec.rb
+++ spec/views/exchange_rates/_document_detail.html.erb_spec.rb
@@ -3,15 +3,16 @@ require 'spec_helper'
 RSpec.describe 'exchange_rates/_document_detail', type: :view do
   subject { render }
 
-  let(:period) { build(:exchange_rate_period) }
+  let(:period) { build(:exchange_rate_period, has_exchange_rates: true) }
 
   before do
     allow(view).to receive(:period) { period }
+    allow(view).to receive(:type_label).and_return('spot')
   end
 
-  it { is_expected.to have_css 'h3', text: "June #{period.year} monthly exchange rates" }
+  it { is_expected.to have_css 'h3', text: "June #{period.year} spot exchange rates" }
 
   it { is_expected.to have_link('CSV', href: '/exchange_rates/csv/exrates-monthly-0623.csv'), count: 2 }
 
-  it { is_expected.to have_css 'p', text: 'View online | CSV 2.0 KB | CSV 2.0 KB', normalize_ws: true }
+  it { is_expected.to have_link('View online', href: "/exchange_rates/view/#{period.year}-#{period.month}") }
 end
