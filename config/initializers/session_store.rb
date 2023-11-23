8c2dbe952acd615c4acf7578d008da9d2d4dbdb0
Amber Cronin <amber@amber.vision>
2023-11-23 15:12:38 +0000

    HOTT-4437: Set Secure Flag on Cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-TradeTariffFrontend::Application.config.session_store :cookie_store, key: '_tradetarifffrontend_session'
+TradeTariffFrontend::Application.config.session_store(
+  :cookie_store,
+  key: '_tradetarifffrontend_session',
+  secure: Rails.env.production?,
+)
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
