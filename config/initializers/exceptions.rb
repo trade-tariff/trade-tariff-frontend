bd452e1d8c6f04afcbe8b4efd5dc18936f4e0645
Alexdesi <alexdesi@gmail.com>
2023-12-11 14:30:39 +0000

    WIP - remove rescue errors for debugging purposes.

--- config/initializers/exceptions.rb
+++ config/initializers/exceptions.rb
@@ -1,10 +1,1 @@
-ActionDispatch::ExceptionWrapper.rescue_responses.merge!(
-  'Faraday::ResourceNotFound' => :not_found,
-  'WizardSteps::UnknownStep' => :not_found,
-  'ActionView::MissingTemplate' => :not_found,
-  'ActionController::UnknownFormat' => :not_found,
-  'AbstractController::ActionNotFound' => :not_found,
-  'URI::InvalidURIError' => :not_found,
-  'TradeTariffFrontend::FeatureUnavailable' => :not_found,
-  'TradeTariffFrontend::MaintenanceMode' => :service_unavailable,
-)
+ActionDispatch::ExceptionWrapper.rescue_responses = {}
