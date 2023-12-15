08dfed3f1d3a126c93b96a95b90e8ab23e0b2b5d
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2023-12-15 15:48:20 +0000

    HOTT-4629: Extend Frontend ErrorsController

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -26,12 +26,64 @@ class ErrorsController < ApplicationController
   end
 
   def maintenance
+    # shouldn't this be service_unavailable as that's what exception is mapped to
+    # 'TradeTariffFrontend::MaintenanceMode' => :service_unavailable,
     @skip_news_banner = true
 
     respond_to do |format|
       format.html { render status: :service_unavailable }
       format.json { render json: { error: 'Maintenance mode' }, status: :service_unavailable }
       format.all { render status: :service_unavailable, plain: 'Maintenance mode' }
+    end
+  end
+
+  def unprocessable_entity
+    @skip_news_banner = true
+
+    respond_to do |format|
+      format.html { render status: :unprocessable_entity }
+      format.json { render json: { error: 'Unprocessable entity' }, status: :unprocessable_entity }
+      format.all { render status: :unprocessable_entity, plain: 'Unprocessable entity' }
+    end
+  end
+
+  def bad_request
+    @skip_news_banner = true
+
+    respond_to do |format|
+      format.html { render status: :bad_request }
+      format.json { render json: { error: 'Bad request' }, status: :bad_request }
+      format.all { render status: :bad_request, plain: 'Bad request' }
+    end
+  end
+
+  def not_implemented
+    @skip_news_banner = true
+
+    respond_to do |format|
+      format.html { render status: :bad_request }
+      format.json { render json: { error: 'Bad request: Not implemented' }, status: :bad_request }
+      format.all { render status: :bad_request, plain: 'Bad request: Not Implemented' }
+    end
+  end
+
+  def method_not_allowed
+    @skip_news_banner = true
+
+    respond_to do |format|
+      format.html { render status: :bad_request }
+      format.json { render json: { error: 'Bad request: Method not allowed' }, status: :bad_request }
+      format.all { render status: :bad_request, plain: 'Bad request' }
+    end
+  end
+
+  def not_acceptable
+    @skip_news_banner = true
+
+    respond_to do |format|
+      format.html { render status: :bad_request }
+      format.json { render json: { error: 'Bad request: Not acceptable' }, status: :bad_request }
+      format.all { render status: :bad_request, plain: 'Bad request' }
     end
   end
 
