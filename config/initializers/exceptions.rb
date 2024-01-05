fbe5f856470e16594ebdbd222377c3e1fdc706de
Alexdesi <alexdesi@gmail.com>
2024-01-05 13:53:36 +0000

    HOTT-4457: Add missing exceptions and fix specs.

--- config/initializers/exceptions.rb
+++ config/initializers/exceptions.rb
@@ -1,1 +1,10 @@
-ActionDispatch::ExceptionWrapper.rescue_responses = {}
+ActionDispatch::ExceptionWrapper.rescue_responses.merge!(
+  'Faraday::ResourceNotFound' => :not_found,
+  'WizardSteps::UnknownStep' => :not_found,
+  'ActionView::MissingTemplate' => :not_found,
+  'ActionController::UnknownFormat' => :not_found,
+  'AbstractController::ActionNotFound' => :not_found,
+  'URI::InvalidURIError' => :not_found,
+  'TradeTariffFrontend::FeatureUnavailable' => :not_found,
+  'TradeTariffFrontend::MaintenanceMode' => :service_unavailable,
+)
