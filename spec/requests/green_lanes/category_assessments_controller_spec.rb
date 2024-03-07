49726a1515aafc4834ddd095ea5b79b2ce5000e8
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2024-03-07 11:15:33 +0000

    fix review comments

--- spec/requests/green_lanes/category_assessments_controller_spec.rb
+++ spec/requests/green_lanes/category_assessments_controller_spec.rb
@@ -16,16 +16,35 @@ RSpec.describe GreenLanes::CategoryAssessmentsController, type: :request do
                          .and_return goods_nomenclature
     end
 
-    let(:make_request) { post green_lanes_category_assessments_path, params: { green_lanes_category_assessment_search: { commodity_code: goods_nomenclature.goods_nomenclature_item_id } } }
+    let(:make_request) do
+      post green_lanes_category_assessments_path, params: {
+        green_lanes_category_assessment_search: {
+          commodity_code: goods_nomenclature.goods_nomenclature_item_id,
+        },
+      }
+    end
 
     it { is_expected.to have_http_status :ok }
     it { is_expected.to have_attributes content_type: %r{text/html} }
   end
 
   describe 'GET #show' do
-    let(:make_request) { get green_lanes_category_assessments_path }
+    context 'when green lanes is allowed' do
+      let(:make_request) { get green_lanes_category_assessments_path }
 
-    it { is_expected.to have_http_status :ok }
-    it { is_expected.to have_attributes content_type: %r{text/html} }
+      it { is_expected.to have_http_status :ok }
+      it { is_expected.to have_attributes content_type: %r{text/html} }
+    end
+
+    context 'when green lanes is not allowed' do
+      before do
+        allow(TradeTariffFrontend).to receive(:green_lane_allowed?).and_return false
+      end
+
+      let(:make_request) { get green_lanes_category_assessments_path }
+
+      it { is_expected.to have_http_status :not_found }
+      it { is_expected.to have_attributes content_type: %r{text/html} }
+    end
   end
 end
