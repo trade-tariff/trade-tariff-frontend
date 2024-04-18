7c12313aa0b3ac1f6a2f83c7e90ef0d0288833b9
Hadleigh Wallenberg <hadleigh.wallenberg@transformuk.com>
2024-04-18 13:48:10 +0100

    OTT-29 Updated trading_partners select country page

--- spec/views/trading_partners/show.html.erb_spec.rb
+++ spec/views/trading_partners/show.html.erb_spec.rb
@@ -8,4 +8,22 @@ RSpec.describe 'trading_partners/show', type: :view, vcr: { cassette_name: 'geog
   describe 'header' do
     it { is_expected.to have_css 'header span', text: /UK Integrated/ }
   end
+
+  describe 'Tariff specific change xi' do
+    before do
+      allow(TradeTariffFrontend::ServiceChooser).to \
+        receive(:service_choice).and_return 'xi'
+    end
+
+    it { is_expected.to have_css 'form', text: / View EU / }
+  end
+
+  describe 'Tariff specific change uk' do
+    before do
+      allow(TradeTariffFrontend::ServiceChooser).to \
+        receive(:service_choice).and_return 'uk'
+    end
+
+    it { is_expected.not_to have_css 'form', text: / View EU / }
+  end
 end
