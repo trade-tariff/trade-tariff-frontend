f6648e4aac67c0ab39a1990d98d50672859964cb
Hadleigh Wallenberg <hadleigh.wallenberg@transformuk.com>
2024-04-18 15:40:51 +0100

    Made RSpec changes following review

--- spec/views/trading_partners/show.html.erb_spec.rb
+++ spec/views/trading_partners/show.html.erb_spec.rb
@@ -9,21 +9,13 @@ RSpec.describe 'trading_partners/show', type: :view, vcr: { cassette_name: 'geog
     it { is_expected.to have_css 'header span', text: /UK Integrated/ }
   end
 
-  describe 'Tariff specific change xi' do
-    before do
-      allow(TradeTariffFrontend::ServiceChooser).to \
-        receive(:service_choice).and_return 'xi'
-    end
-
+  describe 'Tariff specific change' do
+    include_context 'with XI service'
     it { is_expected.to have_css 'form', text: / View EU / }
   end
 
-  describe 'Tariff specific change uk' do
-    before do
-      allow(TradeTariffFrontend::ServiceChooser).to \
-        receive(:service_choice).and_return 'uk'
-    end
-
+  describe 'Tariff specific change' do
+    include_context 'with UK service'
     it { is_expected.not_to have_css 'form', text: / View EU / }
   end
 end
