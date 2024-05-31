9d4fc5ab2e3706f40644f0c436c740855c291542
Alexdesi <alexdesi@gmail.com>
2024-05-31 15:08:55 +0100

    GL-531: Add VCR support on green lanes check_moving_requirements controller spec.

--- spec/requests/green_lanes/check_moving_requirements_controller_spec.rb
+++ spec/requests/green_lanes/check_moving_requirements_controller_spec.rb
@@ -38,12 +38,12 @@ RSpec.describe GreenLanes::CheckMovingRequirementsController, type: :request do
     it { is_expected.to have_http_status :ok }
   end
 
-  describe 'PUT #update' do
+  describe 'PUT #update', vcr: { cassette_name: 'green_lanes/get_goods_nomenclatures_200' } do
     context 'when all the form values are correct' do
       let(:make_request) do
         put green_lanes_check_moving_requirements_path, params: {
           green_lanes_moving_requirements_form: {
-            commodity_code: '1234567890',
+            commodity_code: '6203000000',
             country_of_origin: 'IT',
             'moving_date(3i)' => '3',
             'moving_date(2i)' => '2',
@@ -52,7 +52,13 @@ RSpec.describe GreenLanes::CheckMovingRequirementsController, type: :request do
         }
       end
 
-      it { is_expected.to redirect_to(result_green_lanes_check_moving_requirements_path) }
+      it do
+        expect(make_request).to redirect_to(result_green_lanes_check_moving_requirements_path(
+                                              green_lanes_moving_requirements_form: {
+                                                commodity_code: '6203000000', country_of_origin: 'IT', moving_date: '2022-02-03'
+                                              },
+                                            ))
+      end
     end
 
     context 'when a value is missing or incorrect' do
