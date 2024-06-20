ae4021d4ddf1820f6ebc1d9d701cf7706c31ba72
Alexdesi <alexdesi@gmail.com>
2024-06-20 16:44:19 +0100

    GL-580: Simplify logic to select results.

--- app/services/green_lanes/determine_category.rb
+++ app/services/green_lanes/determine_category.rb
@@ -7,24 +7,21 @@ module GreenLanes
     end
 
     def call
-      return [:cat_3] if category_assessments.empty? # Result 3
+      return [:cat_3] if category_assessments.empty?  # Result 3
+      return [:cat_1] if cat1_without_exemptions.any? # Result 1
 
-      if cat1_without_exemptions.any?
-        [:cat_1] # Result 1
-      elsif cat2_without_exemptions.any?
+      if cat2_without_exemptions.any?
         if cat1_with_exemptions.any?
           %i[cat_1 cat_2] # Result 4
         else
           [:cat_2] # Result 2
         end
+      elsif cat1_with_exemptions.any? && cat2_with_exemptions.any?
+        %i[cat_1 cat_2 cat_3] # Result 5
       elsif cat1_with_exemptions.any?
-        if cat2_with_exemptions.any?
-          %i[cat_1 cat_2 cat_3] # Result 5
-        else
-          %i[cat_1 cat_3] # Result 6
-        end
+        %i[cat_1 cat_3] # Result 6
       elsif cat2_with_exemptions.any?
-        %i[cat_2 cat_3]
+        %i[cat_2 cat_3] # Result 7
       end
     end
 
