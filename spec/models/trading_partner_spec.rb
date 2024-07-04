43855fdcc33b4d6138be317b7dd3c23590ce63dc
Hadleigh Wallenberg <hadleigh.wallenberg@transformuk.com>
2024-07-04 16:42:54 +0100

    Refactored js-country-picker-select into stimulus controller and utility class. Updated trading_partner to validate blank as all countries when javascript disabled.

--- spec/models/trading_partner_spec.rb
+++ spec/models/trading_partner_spec.rb
@@ -12,16 +12,22 @@ RSpec.describe TradingPartner, vcr: { cassette_name: 'geographical_areas#countri
   describe 'errors' do
     before { trading_partner.valid? }
 
-    context 'when a valid date is passed' do
+    context 'when a valid country is passed' do
       let(:attributes) { { 'country' => 'IT' } }
 
       it { expect(trading_partner.errors.messages).to be_empty }
     end
 
-    context 'when an invalid date is passed' do
+    context 'when an invalid country is passed' do
       let(:attributes) { { 'country' => 'FOO' } }
 
       it { expect(trading_partner.errors.messages[:country]).to include('Select a country') }
+    end
+
+    context 'when all countries (blank) is selected' do
+      let(:attributes) { { 'country' => ' ' } }
+
+      it { expect(trading_partner.errors.messages).to be_empty }
     end
   end
 end
