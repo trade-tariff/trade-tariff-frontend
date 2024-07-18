425d106b39cc144fbb827c1db6da3d70e8b7e6f5
Hadleigh Wallenberg <hadleigh.wallenberg@transformuk.com>
2024-07-18 16:16:11 +0100

    Refactored js-commodity-picker-select into stimulus controller and utility class

--- spec/javascript/commodities.spec.js
+++ spec/javascript/commodities.spec.js
@@ -13,7 +13,6 @@ test('GOVUK.tariff public has expected public methods', () => {
     'measuresTable',
     'searchForm',
     'searchHighlight',
-    'selectBoxes',
     'tabLinkFocuser',
     'tablePopup',
     'tabs',
@@ -21,32 +20,5 @@ test('GOVUK.tariff public has expected public methods', () => {
 
   items.forEach( (item) => {
     expect(GOVUK.tariff).toHaveProperty(item);
-  });
-});
-
-describe('searchForm', () => {
-  const autocompleteSelector = '.autocomplete__wrapper input';
-
-  const commodityPickerTemplate = `
-    <div class="search-header js-search-header">
-      <input type="text" name="q" class="js-commodity-picker-target" />
-      <div class='js-commodity-picker-select js-show'></div>
-
-      <input type="input" name="day" id="tariff_date_day" />
-      <input type="input" name="month" id="tariff_date_month" />
-      <input type="input" name="year" id="tariff_date_year" />
-
-      <input type="submit" value="Search" />
-    </div>
-  `;
-
-  beforeEach(() => {
-    document.body.innerHTML = commodityPickerTemplate;
-    GOVUK.tariff.selectBoxes.initialize(document.body);
-  });
-
-  it('loads the autocomplete', () => {
-    const autocompleteWidgets = document.querySelectorAll(autocompleteSelector);
-    expect(autocompleteWidgets.length).toBe(1);
   });
 });
