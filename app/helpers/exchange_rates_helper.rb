8d6e31df0b04db5933678b91578f9bd4a5d06a10
Hadleigh Wallenberg <hadleigh.wallenberg@transformuk.com>
2024-07-22 17:28:55 +0100

    OTT-427 Update the 'related information' heading and links

--- app/helpers/exchange_rates_helper.rb
+++ app/helpers/exchange_rates_helper.rb
@@ -7,18 +7,18 @@ module ExchangeRatesHelper
     case type
     when 'monthly'
       [
-        list_item_for('Average rates', :average),
-        list_item_for('Spot rates', :spot),
+        list_item_for('Average currency exchange rates', :average),
+        list_item_for('Spot currency exchange rates', :spot),
       ]
     when 'spot'
       [
-        list_item_for('Average rates', :average),
-        list_item_for('Monthly rates', :monthly),
+        list_item_for('Average currency exchange rates', :average),
+        list_item_for('Monthly currency exchange rates', :monthly),
       ]
     when 'average'
       [
-        list_item_for('Monthly rates', :monthly),
-        list_item_for('Spot rates', :spot),
+        list_item_for('Monthly currency exchange rates', :monthly),
+        list_item_for('Spot currency exchange rates', :spot),
       ]
     end.join.html_safe
   end
