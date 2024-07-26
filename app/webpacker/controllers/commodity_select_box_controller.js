32a49c11c20b8b6d56b446a45d35eadff70df310
Hadleigh Wallenberg <hadleigh.wallenberg@transformuk.com>
2024-07-26 11:51:54 +0100

    WIP Ensure Search with event handlers

--- app/webpacker/controllers/commodity_select_box_controller.js
+++ app/webpacker/controllers/commodity_select_box_controller.js
@@ -18,7 +18,7 @@ export default class extends Controller {
     this.resourceIdHidden.value = '';
 
     this.#initializeAutocomplete();
-    this.#initializeEventListeners();
+    this.#attachEventListeners();
   }
 
   // templates can't be private functions as they won't work with dynamic accessibleAutocomplete.enhanceSelectElement
@@ -71,57 +71,78 @@ export default class extends Controller {
     });
   }
 
-  #initializeEventListeners() {
-    this.inputElement.on('keydown', 'li[id^="q__option--"]', this.#handleSubmitEvent);
-    this.inputElement.on('keydown', 'input[type="text"]', this.#handleSubmitEvent);
-    this.inputElement.on('click', 'li[id^="q__option--"]', this.#handleSubmitEvent);
-    this.inputElement.addEventListener('change', () => {
-      if (this.inputElement.value === '') {
-        this.resourceIdHidden.value = '';
+  #attachEventListeners() {
+    this.inputElementForEventHandling = document.getElementById('q');
+    this.inputElementForEventHandling.addEventListener('keydown', (event) => {
+      if (event.target.matches('input[type="text"]')) {
+        this.#handleSubmitEvent(event);
       }
     });
+
+    //Selecting a list option is handled by accessibleAutocomplete onConfirm now
+    // this.inputElementForEventHandling.addEventListener('keydown', (event) => {
+    //   if (event.target.matches('li[id^="q__option--"]')) {
+    //     this.#handleSubmitEvent(event);
+    //   }
+    // });
+
+    //Clicking a list option is handled by accessibleAutocomplete onConfirm now
+    // this.inputElementForEventHandling.addEventListener('click', (event) => {
+    //   if (event.target.matches('li[id^="q__option--"]')) {
+    //     this.#handleSubmitEvent(event);
+    //   }
+    // });
   }
+
   // Avoid the default keydown behaviour of the autocomplete library and
   // auto submit on Mousclick, Enter or Tab ourselves
   // when an element receives Enter, the form is submitted
   // when an element selected with arrow keys, the form is not submitted
-  #handleSubmitEvent(ev) {
-    if (ev.type === 'click' || ev.key === 'Enter' || ev.key === 'Tab') {
-      const form = $(element).parents('form');
-      const suggestionType = $(ev.target).find('[data-suggestion-type]').data('suggestion-type');
-      const resourceId = $(ev.target).find('[data-resource-id]').data('resource-id');
-      let text = $(ev.target).text();
+  #handleSubmitEvent(event) {
 
-      ev.preventDefault();
+    // NOT SURE IF THE COMMENTED OUT CODE BELOW IS REQUIRED
+    // FOR EXAMPLE THE SEARCH FOR 'TEA' AND HITTING ENTER HAS THE SAME EFFECT AS CLICKING ON IT IN THE DROPDOWN.
+    // CLICK is handled by onConfirm in accessibleAutocomplete now.
+    if (event.key === 'Enter' || event.key === 'Tab') {
+        this.inputElement.value = event.target.value;
+        let text = event.target.value;
+        Utility.commoditySelectorOnConfirm(text, this.options, this.resourceIdHidden, this.inputElement);
 
-      if (text === '') {
-        text = $(element).find('input[type="text"]').val();
-      }
+    //   const form = $(element).parents('form');
+    //   const suggestionType = $(ev.target).find('[data-suggestion-type]').data('suggestion-type');
+    //   const resourceId = $(ev.target).find('[data-resource-id]').data('resource-id');
+    //   let text = $(ev.target).text();
 
-      if (suggestionType) {
-        text = text.replace(suggestionType, '');
-      }
+    //   ev.preventDefault();
 
-      // accessible-autocomplete adds the index of the options into
-      // the option text on ios for some reason
-      // That breaks search so strip it back out
-      // FIXME: Solve event handling so we can just handle submission
-      // in onConfirm and avoid all this complexity
-      if (isIosDevice()) {
-        text = text.replace(/ \d+ of \d+$/, '');
-      }
+    //   if (text === '') {
+    //     text = $(element).find('input[type="text"]').val();
+    //   }
 
-      form.find('.js-commodity-picker-target').val(text);
+    //   if (suggestionType) {
+    //     text = text.replace(suggestionType, '');
+    //   }
 
-      if (resourceId) {
-        form.find('.js-commodity-picker-resource-id').val(resourceId);
-      }
+    //   // accessible-autocomplete adds the index of the options into
+    //   // the option text on ios for some reason
+    //   // That breaks search so strip it back out
+    //   // FIXME: Solve event handling so we can just handle submission
+    //   // in onConfirm and avoid all this complexity
+    //   if (isIosDevice()) {
+    //     text = text.replace(/ \d+ of \d+$/, '');
+    //   }
 
-      form.submit();
+    //   form.find('.js-commodity-picker-target').val(text);
+
+    //   if (resourceId) {
+    //     form.find('.js-commodity-picker-resource-id').val(resourceId);
+    //   }
+
+    //   form.submit();
     }
   }
 
-  #isIosDevice() {
-    return typeof window.navigator !== 'undefined' && !!(window.navigator.userAgent.match(/(iPod|iPhone|iPad)/g) && navigator.userAgent.match(/AppleWebKit/g));
-  }
+  // #isIosDevice() {
+  //   return typeof window.navigator !== 'undefined' && !!(window.navigator.userAgent.match(/(iPod|iPhone|iPad)/g) && navigator.userAgent.match(/AppleWebKit/g));
+  // }
 }
