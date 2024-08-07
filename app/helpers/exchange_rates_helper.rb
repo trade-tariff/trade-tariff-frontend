6fd27401d13c67abb6a7bf9d887f272ee5bd3c41
Hadleigh Wallenberg <hadleigh.wallenberg@transformuk.com>
2024-08-07 16:36:25 +0100

    Further UX changes after QA checks

--- app/helpers/exchange_rates_helper.rb
+++ app/helpers/exchange_rates_helper.rb
@@ -7,18 +7,18 @@ module ExchangeRatesHelper
     case type
     when 'monthly'
       [
-        list_item_for('Average currency exchange rates', :average),
-        list_item_for('Spot currency exchange rates', :spot),
+        list_item_for('Currency exchange average rates', :average),
+        list_item_for('Currency exchange spot rates', :spot),
       ]
     when 'spot'
       [
-        list_item_for('Average currency exchange rates', :average),
-        list_item_for('Monthly currency exchange rates', :monthly),
+        list_item_for('Currency exchange average rates', :average),
+        list_item_for('Currency exchange monthly rates', :monthly),
       ]
     when 'average'
       [
-        list_item_for('Monthly currency exchange rates', :monthly),
-        list_item_for('Spot currency exchange rates', :spot),
+        list_item_for('Currency exchange monthly rates', :monthly),
+        list_item_for('Currency exchange spot rates', :spot),
       ]
     end.join.html_safe
   end
