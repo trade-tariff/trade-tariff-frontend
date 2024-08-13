1433ecded0439b55a5a2a60212d2864e1d823859
Sam Johnson <sh.johnson@me.com>
2024-08-13 17:17:18 +0100

    fix when multiple exemptions shown showing them all as exempt

--- app/helpers/green_lanes_helper.rb
+++ app/helpers/green_lanes_helper.rb
@@ -1,53 +1,43 @@
 module GreenLanesHelper
   def exemption_checkbox_checked?(category_assessment_id, exemption_code)
-    cas_checked = category_assessments_checked(category_assessment_id)
-
-    return if cas_checked.nil?
-
-    cas_checked.include?(exemption_code)
+    category_assessments_checked(category_assessment_id)&.include?(exemption_code)
   end
 
   def exemption_checkbox_none?(category_assessment_id)
-    cas_checked = category_assessments_checked(category_assessment_id)
-
-    return if cas_checked.nil?
-
-    cas_checked.include?('none')
+    exemption_checkbox_checked?(category_assessment_id, 'none')
   end
 
   def render_exemptions_or_no_card(category, assessments)
     no_exemptions = assessments.public_send("no_cat#{category}_exemptions")
     exemptions_met = assessments.public_send("cat_#{category}_exemptions_met")
-
     total_exemptions = assessments.public_send("cat_#{category}_exemptions")
 
     all_exemptions_met = total_exemptions.count == exemptions_met.count
 
-    if no_exemptions || !all_exemptions_met
-      render('category_assessments_card', category:)
-    else
-      render 'exemptions_card', category:
-    end
+    template = no_exemptions || !all_exemptions_met ? 'category_assessments_card' : 'exemptions_card'
+    render(template, category:)
   end
 
-  def exemption_met?(category, category_assessment, answers)
+  def exemption_met?(exemption_code, category, category_assessment, answers)
     return false if answers.blank?
 
-    category = category.to_s
-
-    category_assessment_answer = answers.dig(category, category_assessment.category_assessment_id.to_s)
-
-    category_assessment_answer.present? && category_assessment_answer != %w[none]
+    category_assessment_answer = dig_category_answer(answers, category, category_assessment.category_assessment_id)
+    category_assessment_answer.present? && category_assessment_answer != %w[none] && category_assessment_answer.include?(exemption_code)
   end
 
   def all_exemptions_met?(category, category_assessments, answers)
-    category_assessments.all? { |ca| exemption_met?(category, ca, answers) }
+    return false if answers.blank?
+
+    category_assessments.all? do |ca|
+      category_assessment_answer = dig_category_answer(answers, category, ca.category_assessment_id)
+      category_assessment_answer.present? && category_assessment_answer != %w[none]
+    end
   end
 
   def render_exemptions(assessments, category)
     if category.to_s == '3'
-      render_all_exemptions(assessments) if assessments.cat_1_exemptions_met || assessments.cat_2_exemptions_met
-    elsif assessments.cat_1_exemptions_met || assessments.cat_2_exemptions_met || !assessments.no_cat1_exemptions
+      render_all_exemptions(assessments) if any_exemptions_met?(assessments)
+    elsif any_exemptions_met?(assessments) || !assessments.no_cat1_exemptions
       result = render_exemptions_or_no_card(1, assessments)
       result += render_exemptions_or_no_card(2, assessments) unless assessments.cat_1_exemptions_not_met
       result
@@ -69,9 +59,14 @@ module GreenLanesHelper
   end
 
   def category_assessments_checked(category_assessment_id)
-    exemptions = params[:exemptions]
-    return if exemptions.nil? || exemptions[:category_assessments_checked].nil?
+    params.dig(:exemptions, :category_assessments_checked, category_assessment_id.to_s)
+  end
 
-    exemptions[:category_assessments_checked][category_assessment_id.to_s]
+  def dig_category_answer(answers, category, category_assessment_id)
+    answers.dig(category.to_s, category_assessment_id.to_s)
+  end
+
+  def any_exemptions_met?(assessments)
+    assessments.cat_1_exemptions_met || assessments.cat_2_exemptions_met
   end
 end
