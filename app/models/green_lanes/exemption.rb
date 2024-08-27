1e69c2ced6406bf52d36e11554a8359f1f83e243
Sam Johnson <sh.johnson@me.com>
2024-08-27 16:06:50 +0100

    Move the display code inside the exemption model

--- app/models/green_lanes/exemption.rb
+++ app/models/green_lanes/exemption.rb
@@ -3,5 +3,9 @@ module GreenLanes
     include XiOnlyApiEntity
 
     attr_accessor :code, :description, :formatted_description
+
+    def display_code
+      code.start_with?('WFE') ? '' : code
+    end
   end
 end
