862c4c75dc9571c49b5f723a686f91dcbd12bb03
rasika.abeyrathna <rasikasri@gmail.com>
2024-10-16 17:23:24 +0100

    Fix test

--- app/services/green_lanes/validate_declarable_goods_nomenclature.rb
+++ app/services/green_lanes/validate_declarable_goods_nomenclature.rb
@@ -5,7 +5,7 @@ module GreenLanes
     end
 
     def call
-      goods_nomenclature = ::GoodsNomenclature.find(@params[:commodity_code],{as_of: @params[:moving_date] })
+      goods_nomenclature = ::GoodsNomenclature.find(@params[:commodity_code], { as_of: @params[:moving_date] })
       raise Faraday::ResourceNotFound unless goods_nomenclature.declarable
     end
   end
