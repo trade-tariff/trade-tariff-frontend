f4b42486276c75fdf41d103c495873db9707d5ce
Hadleigh Wallenberg <hadleigh.wallenberg@transformuk.com>
2025-01-16 15:09:04 +0000

    Removed unused js variable to fix linting error

--- app/webpacker/src/javascripts/google-tag-manager-loader.js
+++ app/webpacker/src/javascripts/google-tag-manager-loader.js
@@ -6,7 +6,6 @@ if (cookieManager.usage()) {
   (function(w, d, s, l, i) {
     w[l] = w[l] || [];
     w[l].push({'gtm.start': new Date().getTime(), 'event': 'gtm.js'});
-    const f = d.getElementsByTagName(s)[0];
     const j = d.createElement(s);
     const dl = l != 'dataLayer' ? '&l=' + l : '';
     j.async = true;
