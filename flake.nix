eee8fc769e669dab04fb0767f324ef57d59a886c
William Fish <william.michael.fish@gmail.com>
2025-01-29 10:17:48 +0000

    wf/simplify-nix: Simplify nix configuration
    Stop relying on a flakey/noisy gemset implementation and move to gitignored writeable directories

--- flake.nix
+++ flake.nix
@@ -1,104 +1,66 @@
 {
-  description = "trade-tariff-frontend";
-
-  nixConfig = {
-    extra-substituters = "https://nixpkgs-ruby.cachix.org";
-    extra-trusted-public-keys = "nixpkgs-ruby.cachix.org-1:vrcdi50fTolOxWCZZkw0jakOnUI1T19oYJ+PRYdK4SM=";
-  };
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?rev=205fd4226592cc83fd4c0885a3e4c9c400efabb5";
-    ruby-nix.url = "github:inscapist/ruby-nix";
-    # a fork that supports platform dependant gem
-    bundix = {
-      url = "github:inscapist/bundix/main";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs-ruby = {
+      url = "github:bobvanderlinden/nixpkgs-ruby";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
-    fu.url = "github:numtide/flake-utils";
-    bob-ruby.url = "github:bobvanderlinden/nixpkgs-ruby";
-    bob-ruby.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      fu,
-      ruby-nix,
-      bundix,
-      bob-ruby,
-    }:
-    with fu.lib;
-    eachDefaultSystem (
-      system:
+  outputs = { self, nixpkgs, flake-utils, nixpkgs-ruby }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ bob-ruby.overlays.default ];
+          system = system;
           config.allowUnfree = true;
+          overlays = [nixpkgs-ruby.overlays.default];
         };
-        rubyNix = ruby-nix.lib pkgs;
 
-        gemset = if builtins.pathExists ./gemset.nix then import ./gemset.nix else { };
+        rubyVersion = let
+          rubyRegex = "^ruby (.*)$";
+          toolVersionsLines = builtins.split "\n" (builtins.readFile ./.tool-versions);
+          rubyLine = builtins.head (builtins.filter (line: builtins.match rubyRegex (builtins.toString line) != null) toolVersionsLines);
+          versionMatch = builtins.match rubyRegex rubyLine;
+        in builtins.elemAt versionMatch 0;
+        ruby = pkgs."ruby-${rubyVersion}";
 
-        # If you want to override gem build config, see
-        #   https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/ruby-modules/gem-config/default.nix
-        gemConfig = { };
+        lint = pkgs.writeScriptBin "lint" ''
+          changed_files=$(git diff --name-only --diff-filter=ACM --merge-base main)
 
-        # Read Ruby version from the .ruby-version file
-        rubyVersion = builtins.readFile ./.ruby-version;
-
-        # Remove newline or extra whitespace characters
-        sanitizedRubyVersion = builtins.substring 0 (builtins.stringLength rubyVersion - 1) rubyVersion;
-
-        # See available versions here: https://github.com/bobvanderlinden/nixpkgs-ruby/blob/master/ruby/versions.json
-        ruby = pkgs."ruby-${sanitizedRubyVersion}";
-
-        # Running bundix would regenerate `gemset.nix`
-        bundixcli = bundix.packages.${system}.default;
-
-        # Use these instead of the original `bundle <mutate>` commands
-        bundleLock = pkgs.writeShellScriptBin "bundle-lock" ''
-          export BUNDLE_PATH=vendor/bundle
-          bundle lock
-        '';
-        bundleUpdate = pkgs.writeShellScriptBin "bundle-update" ''
-          export BUNDLE_PATH=vendor/bundle
-          bundle lock --update
+          bundle exec rubocop --autocorrect-all --force-exclusion $changed_files Gemfile
         '';
         chrome = pkgs.writeShellScriptBin "chrome" ''
           binary=$(find ${pkgs.google-chrome.outPath} -type f -name 'google-chrome-stable')
           exec $binary "$@"
         '';
-      in
-      rec {
-        inherit
-          (rubyNix {
-            inherit gemset ruby;
-            name = "trade-tariff-frontend";
-            gemConfig = pkgs.defaultGemConfig // gemConfig;
-          })
-          env
-          ;
 
-        devShells = rec {
-          default = dev;
-          dev = pkgs.mkShell {
-            buildInputs =
-              [
-                env
-                bundixcli
-                bundleLock
-                bundleUpdate
-                chrome
-              ]
-              ++ (with pkgs; [
-                yarn
-                rufo
-                google-chrome
-              ]);
-          };
+        psychBuildFlags = with pkgs; [
+          "--with-libyaml-include=${libyaml.dev}/include"
+          "--with-libyaml-lib=${libyaml.out}/lib"
+        ];
+      in {
+        devShells.default = pkgs.mkShell {
+          shellHook = ''
+            export GEM_HOME=$PWD/.nix/ruby/$(${ruby}/bin/ruby -e "puts RUBY_VERSION")
+            mkdir -p $GEM_HOME
+
+            export GEM_PATH=$GEM_HOME
+            export PATH=$GEM_HOME/bin:$PATH
+
+            export BUNDLE_BUILD__PSYCH="${
+              builtins.concatStringsSep " " psychBuildFlags
+            }"
+          '';
+
+          buildInputs = [
+            chrome
+            lint
+            ruby
+            pkgs.yarn
+            pkgs.rufo
+          ];
         };
-      }
-    );
+      });
 }
