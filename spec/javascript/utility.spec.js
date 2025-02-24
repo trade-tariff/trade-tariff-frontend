e7708875c28b833d2eb707b555a3d8962ee77e6e
William Fish <william.michael.fish@gmail.com>
2025-02-24 10:28:26 +0000

    BAU: Lint

--- spec/javascript/utility.spec.js
+++ spec/javascript/utility.spec.js
@@ -1,8 +1,8 @@
 /* eslint-env node, jest */
 
-import Utility from "utility";
+import Utility from 'utility';
 
-describe("Utility.countrySelectorOnConfirm", () => {
+describe('Utility.countrySelectorOnConfirm', () => {
   let selectElement;
   let form;
   let anchorInput;
@@ -15,8 +15,8 @@ describe("Utility.countrySelectorOnConfirm", () => {
     // Mock the window location
     delete window.location;
     window.location = {
-      href: "",
-      hash: "#origin",
+      href: '',
+      hash: '#origin',
     };
 
     // Create a mock select element and other necessary DOM elements
@@ -34,11 +34,11 @@ describe("Utility.countrySelectorOnConfirm", () => {
       </form>
     `;
 
-    selectElement = document.querySelector("select");
-    form = selectElement.closest("form");
+    selectElement = document.querySelector('select');
+    form = selectElement.closest('form');
     anchorInput = selectElement
-      .closest(".govuk-fieldset")
-      .querySelector('input[name$="[anchor]"]');
+        .closest('.govuk-fieldset')
+        .querySelector('input[name$="[anchor]"]');
   });
 
   afterEach(() => {
@@ -47,72 +47,72 @@ describe("Utility.countrySelectorOnConfirm", () => {
   });
 
   it('navigates to the URL for "All countries"', () => {
-    const confirmed = "All countries";
+    const confirmed = 'All countries';
 
     Utility.countrySelectorOnConfirm(confirmed, selectElement);
 
-    expect(window.location.href).toBe("/commodities/1234#origin");
+    expect(window.location.href).toBe('/commodities/1234#origin');
   });
 
-  it("sets the select element value and submits the form for a specific country", () => {
+  it('sets the select element value and submits the form for a specific country', () => {
     // Check if form.submit is called
     form.submit = jest.fn();
 
-    const confirmed = "Afghanistan (AF)";
+    const confirmed = 'Afghanistan (AF)';
 
     Utility.countrySelectorOnConfirm(confirmed, selectElement);
 
-    expect(selectElement.value).toBe("AF");
-    expect(anchorInput.value).toBe("origin");
+    expect(selectElement.value).toBe('AF');
+    expect(anchorInput.value).toBe('origin');
     expect(form.submit).toHaveBeenCalled();
   });
 
   it('navigates to the URL with service "xi" for "All countries"', () => {
     // Set the service to 'xi'
-    document.getElementById("trading_partner_service").value = "xi";
+    document.getElementById('trading_partner_service').value = 'xi';
 
-    const confirmed = "All countries";
+    const confirmed = 'All countries';
 
     Utility.countrySelectorOnConfirm(confirmed, selectElement);
 
     expect(window.location.href).toBe(
-      `${window.location.origin}/xi/commodities/1234#origin`,
+        `${window.location.origin}/xi/commodities/1234#origin`,
     );
   });
 });
 
-describe("Utility.fetchCommoditySearchSuggestions", () => {
+describe('Utility.fetchCommoditySearchSuggestions', () => {
   beforeEach(() => {
     jest.clearAllMocks();
-    jest.spyOn(console, "error").mockImplementation(() => {}); // Suppress console.error
+    jest.spyOn(console, 'error').mockImplementation(() => {}); // Suppress console.error
   });
 
-  const query = "wine";
-  const searchSuggestionsPath = "/search-suggestions";
+  const query = 'wine';
+  const searchSuggestionsPath = '/search-suggestions';
   const options = [];
   const populateResults = jest.fn();
 
-  it("fetches suggestions and populates results", async () => {
+  it('fetches suggestions and populates results', async () => {
     const mockResponse = {
       results: [
         {
-          id: "wine",
-          text: "wine",
-          query: "wine",
-          resource_id: "6828",
-          formatted_suggestion_type: "",
+          id: 'wine',
+          text: 'wine',
+          query: 'wine',
+          resource_id: '6828',
+          formatted_suggestion_type: '',
         },
         {
-          id: "red wine",
-          text: "red wine",
-          query: "wine",
-          resource_id: "7273",
-          formatted_suggestion_type: "",
+          id: 'red wine',
+          text: 'red wine',
+          query: 'wine',
+          resource_id: '7273',
+          formatted_suggestion_type: '',
         },
       ],
     };
 
-    const expectedResults = ["wine", "red wine"];
+    const expectedResults = ['wine', 'red wine'];
 
     global.fetch = jest.fn(() =>
       Promise.resolve({
@@ -121,34 +121,34 @@ describe("Utility.fetchCommoditySearchSuggestions", () => {
     );
 
     await Utility.fetchCommoditySearchSuggestions(
-      query,
-      searchSuggestionsPath,
-      options,
-      populateResults,
+        query,
+        searchSuggestionsPath,
+        options,
+        populateResults,
     );
 
-    expect(populateResults).toHaveBeenCalledWith(["wine", "red wine"]);
+    expect(populateResults).toHaveBeenCalledWith(['wine', 'red wine']);
 
     expect(populateResults).toHaveBeenCalledWith(
-      expect.arrayContaining(expectedResults),
+        expect.arrayContaining(expectedResults),
     );
   });
 
-  it("handles fetch error gracefully", async () => {
-    global.fetch = jest.fn(() => Promise.reject(new Error("Network error")));
+  it('handles fetch error gracefully', async () => {
+    global.fetch = jest.fn(() => Promise.reject(new Error('Network error')));
 
     await Utility.fetchCommoditySearchSuggestions(
-      query,
-      searchSuggestionsPath,
-      options,
-      populateResults,
+        query,
+        searchSuggestionsPath,
+        options,
+        populateResults,
     );
 
     expect(populateResults).toHaveBeenCalledWith([]);
   });
 });
 
-describe("Utility.commoditySelectorOnConfirm", () => {
+describe('Utility.commoditySelectorOnConfirm', () => {
   let options;
   let resourceIdHidden;
   let inputElement;
@@ -156,13 +156,13 @@ describe("Utility.commoditySelectorOnConfirm", () => {
 
   beforeEach(() => {
     options = [
-      { id: "wine", text: "wine", suggestion_type: "exact", newOption: true },
+      {id: 'wine', text: 'wine', suggestion_type: 'exact', newOption: true},
       {
-        id: "wine",
-        text: "wine",
-        query: "wine",
-        resource_id: "6828",
-        formatted_suggestion_type: "",
+        id: 'wine',
+        text: 'wine',
+        query: 'wine',
+        resource_id: '6828',
+        formatted_suggestion_type: '',
       },
     ];
 
@@ -173,40 +173,40 @@ describe("Utility.commoditySelectorOnConfirm", () => {
       </form>
     `;
 
-    resourceIdHidden = document.getElementById("resourceIdHidden");
-    inputElement = document.getElementById("inputElement");
-    form = inputElement.closest("form");
+    resourceIdHidden = document.getElementById('resourceIdHidden');
+    inputElement = document.getElementById('inputElement');
+    form = inputElement.closest('form');
 
     form.submit = jest.fn();
   });
 
-  it("sets the resource ID and submits the form when an option is confirmed", () => {
-    const text = { id: "wine", text: "wine" };
+  it('sets the resource ID and submits the form when an option is confirmed', () => {
+    const text = {id: 'wine', text: 'wine'};
 
     Utility.commoditySelectorOnConfirm(
-      text,
-      options,
-      resourceIdHidden,
-      inputElement,
+        text,
+        options,
+        resourceIdHidden,
+        inputElement,
     );
 
-    expect(resourceIdHidden.value).toBe("6828");
-    expect(inputElement.value).toBe("wine");
+    expect(resourceIdHidden.value).toBe('6828');
+    expect(inputElement.value).toBe('wine');
     expect(form.submit).toHaveBeenCalled();
   });
 
-  it("does nothing if the selected option is not found", () => {
-    const text = { id: "3", text: "Unknown Commodity" };
+  it('does nothing if the selected option is not found', () => {
+    const text = {id: '3', text: 'Unknown Commodity'};
 
     Utility.commoditySelectorOnConfirm(
-      text,
-      options,
-      resourceIdHidden,
-      inputElement,
+        text,
+        options,
+        resourceIdHidden,
+        inputElement,
     );
 
-    expect(resourceIdHidden.value).toBe("");
-    expect(inputElement.value).toBe("");
+    expect(resourceIdHidden.value).toBe('');
+    expect(inputElement.value).toBe('');
     expect(form.submit).not.toHaveBeenCalled();
   });
 });
