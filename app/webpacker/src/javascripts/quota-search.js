0b4caf4c82b7d9857bbfc1c26e0acdcb52cb6131
Andrew Pepler <andrew@vertism.com>
2025-03-27 12:10:51 +0000

    refactor to avoid linter errors

--- app/webpacker/src/javascripts/quota-search.js
+++ app/webpacker/src/javascripts/quota-search.js
@@ -1,36 +1,34 @@
 import accessibleAutocomplete from 'accessible-autocomplete';
 
-(function(){
-  var fieldset = $('.js-quota-country-picker');
-  var autocomplete = null;
+(function(global) {
+  const $ = global.jQuery;
+  const fieldset = $('.js-quota-country-picker');
 
-  $('.js-quota-country-picker-select').each(function() {
-    (function(element) {
-      autocomplete = accessibleAutocomplete.enhanceSelectElement({
-        selectElement: element[0],
-        minLength: 2,
-        showAllValues: true,
-        confirmOnBlur: true,
-        dropdownArrow: function() {
-          return "<span class='autocomplete__arrow'></span>";
-        },
-      });
+  $('.js-quota-country-picker-select').each((_, element) => {
+    accessibleAutocomplete.enhanceSelectElement({
+      selectElement: element,
+      minLength: 2,
+      showAllValues: true,
+      confirmOnBlur: true,
+      dropdownArrow: function() {
+        return '<span class=\'autocomplete__arrow\'></span>';
+      },
+    });
 
-      $('#' + element[0].id.replace('-select', '')).on('focus', function(event) {
-        $(event.currentTarget).val('')
-      });
-
-    })($(this));
+    $('#' + element.id.replace('-select', '')).on('focus', (event) => {
+      $(event.currentTarget).val('');
+    });
   });
 
-  fieldset.on('click', 'a.reset-country-picker', function (e) {
+  fieldset.on('click', 'a.reset-country-picker', (e) => {
     $('.js-quota-country-picker-select').val('');
     $('.js-quota-country-picker-select').trigger('blur');
     $('#' + $('.js-quota-country-picker-select').attr('name')).val('All Countries');
 
     return false;
   });
-  $('form.quota-search#new_search').on("submit", function(){
-    $(this).find(':input[type=submit]').prop('disabled', true);
+
+  $('form.quota-search#new_search').on('submit', (event) => {
+    $(event.currentTarget).find(':input[type=submit]').prop('disabled', true);
   });
-})();
+})(window);
