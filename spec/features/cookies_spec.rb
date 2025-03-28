d62c5dc475719a5e5d20ced43e70d68ae4aaa3b9
Andrew Pepler <andrew@vertism.com>
2025-03-28 09:42:46 +0000

    check script presence and remove test duplication

--- spec/features/cookies_spec.rb
+++ spec/features/cookies_spec.rb
@@ -58,7 +58,6 @@ RSpec.feature 'Cookies management', :js do
 
     expect(page).to have_css 'h1', text: 'Cookies on the UK Integrated Online Tariff'
     expect(page).to have_css '#banner', text: /We use some essential cookies/
-    choose 'No, do not use cookies that measure my website use'
     choose 'Yes, use cookies that remember my settings on the site'
     choose 'Use cookies that measure my website use'
     click_on 'Save Changes'
@@ -67,15 +66,19 @@ RSpec.feature 'Cookies management', :js do
     expect(page).to have_css '.govuk-notification-banner h3', text: 'Your cookie settings were saved'
     expect(page).to have_css '#cookies_accepted'
 
-    find(:button, 'Accept additional cookies', visible: true).click
-    expect(cookie_for('cookies_policy')).to eq('remember_settings' => true, 'usage' => true)
-    expect(cookie_for('cookies_preferences_set')).to be_nil
-
+    visit cookies_path
     find(:button, 'Hide this message', visible: true).click
     expect(cookie_for('cookies_policy')).to eq('remember_settings' => true, 'usage' => true)
     expect(cookie_for('cookies_preferences_set')).to eq('value' => true)
-
+    expect(page).to have_selector("script[src*='https://www.googletagmanager.com/gtm.js']", visible: false)
     expect(page).to have_css 'h1', text: 'Cookies on the UK Integrated Online Tariff'
     expect(page).to have_css '#banner', visible: :hidden
+
+    choose 'No, do not use cookies that measure my website use'
+    click_on 'Save Changes'
+    expect(cookie_for('cookies_policy')).to eq('remember_settings' => true, 'usage' => false)
+
+    visit cookies_path
+    expect(page).not_to have_selector("script[src*='https://www.googletagmanager.com/gtm.js']")
   end
 end
