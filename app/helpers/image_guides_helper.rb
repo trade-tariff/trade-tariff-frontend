1fd28690a893f1229a61092b1b173e8d15bb62d7
Neil Middleton <neil.middleton@transformuk.com>
2025-04-25 17:00:32 +0100

    Move images out of webpacker

--- app/helpers/image_guides_helper.rb
+++
@@ -1,9 +0,0 @@
-module ImageGuidesHelper
-  def image_for_guide(guide_name, **kwargs)
-    guide_filename = guide_name.downcase
-
-    image_pack_tag "guides/#{guide_filename}", **kwargs.merge(class: 'image-guide')
-  rescue Webpacker::Manifest::MissingEntryError
-    nil
-  end
-end
