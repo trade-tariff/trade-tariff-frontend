32b307cce21596ec6c5352d6621bed731ab1b62a
Neil Middleton <neil.middleton@transformuk.com>
2025-04-25 17:00:32 +0100

    Move images out of webpacker

--- app/helpers/country_flag_helper.rb
+++ app/helpers/country_flag_helper.rb
@@ -2,9 +2,7 @@ module CountryFlagHelper
   def country_flag_tag(two_letter_country_code, **kwargs)
     two_letter_country_code = two_letter_country_code.downcase
 
-    image_pack_tag "flags/#{two_letter_country_code}.png",
-                   **kwargs.merge(class: 'country-flag')
-  rescue Webpacker::Manifest::MissingEntryError
-    nil
+    image_tag "flags/#{two_letter_country_code}.png",
+              **kwargs.merge(class: 'country-flag')
   end
 end
