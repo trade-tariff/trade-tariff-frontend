e5629f9861ad87b211fd0b4c9101d95c707b595a
Andrew Pepler <1190196+vertism@users.noreply.github.com>
2025-05-09 17:34:16 +0100

    Add encryption service

--- spec/controllers/myott/subscriptions_controller_spec.rb
+++ spec/controllers/myott/subscriptions_controller_spec.rb
@@ -22,6 +22,17 @@ RSpec.describe Myott::SubscriptionsController, type: :controller do
     it { is_expected.to respond_with(:success) }
     it { expect(assigns(:email)).to be_present }
     it { expect(session[:chapter_ids]).to be_nil }
+
+    context 'when current_user exists' do
+      before do
+        allow(controller).to receive(:current_user).and_return({ 'email' => 'test@example.com' })
+      end
+
+      it 'assigns @email with current_user email' do
+        get :dashboard
+        expect(assigns(:email)).to eq('test@example.com')
+      end
+    end
   end
 
   describe 'GET #chapter_selection' do
