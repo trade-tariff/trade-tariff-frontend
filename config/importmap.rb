cf22a2b428963a088e8a297d5d1df53a641bc0e0
Neil Middleton <neil.middleton@transformuk.com>
2025-05-16 11:44:22 +0100

    Alter load order

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,8 +7,6 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin 'core-js/stable', to: 'https://ga.jspm.io/npm:core-js@3.42.0/stable/index.js'
 pin 'govuk-frontend', to: 'https://cdn.jsdelivr.net/npm/govuk-frontend@5.10.0/+esm'
 
-pin_all_from 'app/javascript/controllers', under: 'controllers'
-
 pin 'mark.js' # @8.11.1
 pin 'select2' # @4.1.0
 pin 'jquery' # @3.7.1
@@ -18,3 +16,5 @@ pin 'jquery-migrate' # @3.5.2
 pin 'js-cookie' # @3.0.5
 
 pin_all_from 'app/javascript/src', to: 'src'
+
+pin_all_from 'app/javascript/controllers', under: 'controllers'
