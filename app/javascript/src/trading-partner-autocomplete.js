dac9d6e8e09d9083331cc819e7bccf084101ab56
Neil Middleton <neil.middleton@transformuk.com>
2025-05-21 18:02:41 +0100

    Lint

--- app/javascript/src/trading-partner-autocomplete.js
+++ app/javascript/src/trading-partner-autocomplete.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
   var target = document.querySelector('[id^="trading-partner-country-field"]')
-  if (target != undefined) { 
+  if (target != undefined) {
     window.GOVUK.accessibleAutocomplete.enhanceSelectElement({
       defaultValue: '',
       selectElement: document.querySelector('[id^="trading-partner-country-field"]'),
     })
   }
-}); 
+});
