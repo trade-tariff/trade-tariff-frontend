d805719bcfe24299f5abb0edab1f91643b0b9e0d
Sandya25design <Sandya.karra@transformuk.com>
2025-06-04 16:29:12 +0100

    #Add accessibility testing and HTML report generation with documentation

--- spec/javascript/accessibility/quota_search.spec.js
+++ spec/javascript/accessibility/quota_search.spec.js
@@ -4,7 +4,11 @@ const AxeBuilder = require("@axe-core/playwright").default;
 
 test.describe("Search for quotas", () => {
   test("Validate UK quota Search Results", async ({ page }, testInfo) => {
-    await new LoginPage("/quota_search?order_number=052012", page, testInfo).login();
+    await new LoginPage(
+      "/quota_search?order_number=052012",
+      page,
+      testInfo
+    ).login();
 
     const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
 
