a145d7f8fed1e27bcbada6c137959c3cbc7054e3
Eralia Katsakiori <eralia.katsakiori@outlook.com>
2025-06-06 11:30:38 +0100

    Add link to affected commodities + remove search banner

--- config/routes.rb
+++ config/routes.rb
@@ -59,7 +59,7 @@ Rails.application.routes.draw do
   get '/news/stories/:id', to: 'news_items#show', as: :news_item
   resources :news_items, only: %i[index show], path: '/news'
 
-  get '/live_issues', to: 'live_issues#index', as: :live_issues
+  get '/live_issues', to: 'live_issues#index'
 
   if TradeTariffFrontend.myott?
     namespace :myott, path: 'subscriptions' do
