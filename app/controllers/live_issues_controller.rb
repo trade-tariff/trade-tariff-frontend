ff6fe744a645028c134d42e9676e50252e0e2de1
Eralia Katsakiori <eralia.katsakiori@outlook.com>
2025-06-09 17:00:31 +0100

    Remove last updated from live issues table

--- app/controllers/live_issues_controller.rb
+++ app/controllers/live_issues_controller.rb
@@ -1,5 +1,6 @@
 class LiveIssuesController < ApplicationController
   before_action :disable_switch_service_banner,
+                :disable_last_updated_footnote,
                 :disable_search_form
 
   def index
