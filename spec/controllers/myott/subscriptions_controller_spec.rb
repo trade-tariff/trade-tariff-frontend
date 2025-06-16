9d1df04d9ddc95e6d97bdabceca8ef52a8c45cdb
Hadleigh Wallenberg <hadleigh.wallenberg@transformuk.com>
2025-06-16 18:09:18 +0100

    Added spec

--- spec/controllers/myott/subscriptions_controller_spec.rb
+++ spec/controllers/myott/subscriptions_controller_spec.rb
@@ -28,6 +28,24 @@ RSpec.describe Myott::SubscriptionsController, type: :controller do
       get :invalid
       expect(controller).not_to have_received(:authenticate)
     end
+
+    context 'when a cookie with id_token exists' do
+      before do
+        cookies[:id_token] = 'valid-jwt-token'
+        get :invalid
+      end
+
+      it { is_expected.to redirect_to myott_path }
+    end
+
+    context 'when a cookie with id_token does not exist' do
+      before do
+        cookies[:id_token] = nil
+        get :invalid
+      end
+
+      it { is_expected.not_to redirect_to myott_path }
+    end
   end
 
   describe 'GET #dashboard' do
