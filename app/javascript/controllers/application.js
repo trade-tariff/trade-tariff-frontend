3d441d207b0c9084237cfadeab49834e9571cf6e
Neil Middleton <neil.middleton@transformuk.com>
2025-06-20 13:15:25 +0100

    Ensure the CSRF is up to date in cached forms

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -6,4 +6,8 @@ const application = Application.start()
 application.debug = false
 window.Stimulus   = application
 
+document.addEventListener('DOMContentLoaded', function () {
+  jQuery('input[name=authenticity_token]').val($('meta[name=csrf-token]').attr('content'))
+});
+
 export { application }
