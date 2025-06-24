bcf16dc62a441ff8c7b48b4700254fc37bc01147
Andrew Pepler <1190196+vertism@users.noreply.github.com>
2025-06-24 13:59:57 +0100

    Allow fetching of other cache values

--- spec/support/shared_context/myott.rb
+++ spec/support/shared_context/myott.rb
@@ -6,6 +6,7 @@ RSpec.shared_context 'with cached chapters' do
   let(:chapter3) { build(:chapter, goods_nomenclature_item_id: '03', to_s: 'Fish and crustaceans, molluscs and other aquatic invertebrates') }
 
   before do
+    allow(Rails.cache).to receive(:fetch).and_call_original
     allow(Rails.cache).to receive(:fetch).with('all_sections_chapters', expires_in: 1.day)
       .and_return({ section1 => [chapter1, chapter2], section2 => [chapter3] })
   end
