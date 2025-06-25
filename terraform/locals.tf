3b310dbfdf82eba60118afa76922a246dadf4ede
William Fish <william.michael.fish@gmail.com>
2025-06-25 15:33:12 +0100

    HMRC-1306: Adds workflow to stop development services on a schedule

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,6 +1,7 @@
 locals {
-  secret_value = try(data.aws_secretsmanager_secret_version.this.secret_string, "{}")
-  secret_map   = jsondecode(local.secret_value)
+  has_autoscaler = var.environment == "development" ? false : true
+  secret_value   = try(data.aws_secretsmanager_secret_version.this.secret_string, "{}")
+  secret_map     = jsondecode(local.secret_value)
   secret_env_vars = [
     for key, value in local.secret_map : {
       name  = key
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "service" {
-  source = "git@github.com:trade-tariff/trade-tariff-platform-terraform-modules.git//aws/ecs-service?ref=aws/ecs-service-v1.13.1"
+  source = "git@github.com:trade-tariff/trade-tariff-platform-terraform-modules.git//aws/ecs-service?ref=aws/ecs-service-v1.17.0"
 
   region = var.region
 
@@ -11,9 +11,6 @@ module "service" {
   security_groups           = [data.aws_security_group.this.id]
   target_group_arn          = data.aws_lb_target_group.this.arn
   cloudwatch_log_group_name = "platform-logs-${var.environment}"
-
-  min_capacity = var.min_capacity
-  max_capacity = var.max_capacity
 
   docker_image = "382373577178.dkr.ecr.eu-west-2.amazonaws.com/tariff-frontend-production"
   docker_tag   = var.docker_tag
@@ -28,4 +25,8 @@ module "service" {
   enable_ecs_exec       = true
 
   service_environment_config = local.secret_env_vars
+
+  has_autoscaler = local.has_autoscaler
+  min_capacity   = var.min_capacity
+  max_capacity   = var.max_capacity
 }
