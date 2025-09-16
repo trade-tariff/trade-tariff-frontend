3029f8ce28fbfa274717c0777edf159f00990ebe
Aigul Djon <aigul.djon@gmail.com>
2025-09-16 12:27:07 +0100

    Pin terraform module to the latest version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "service" {
-  source = "git@github.com:trade-tariff/trade-tariff-platform-terraform-modules.git//aws/ecs-service?ref=aws/ecs-service-v1.17.0"
+  source = "git@github.com:trade-tariff/trade-tariff-platform-terraform-modules.git//aws/ecs-service?ref=aws/ecs-service-v1.18.1"
 
   region = var.region
 
