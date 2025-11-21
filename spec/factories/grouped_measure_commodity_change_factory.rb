6207503cb2b911488ce82d16a9557395ef335dbb
Andrew Pepler <1190196+vertism@users.noreply.github.com>
2025-11-21 17:30:04 +0000

    Add view test

--- spec/factories/grouped_measure_commodity_change_factory.rb
+++ spec/factories/grouped_measure_commodity_change_factory.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :grouped_measure_commodity_change, class: 'TariffChanges::GroupedMeasureCommodityChange' do
+    resource_id { 'import_IL__0807190050' }
     count { 2 }
     impacted_measures { { 'Preferential tariff quota' => [] } }
     commodity do
