ac55a14e62ab793d5dba8eee8898ad3fbd657060
William Fish <william.michael.fish@gmail.com>
2025-11-24 21:14:48 +0000

    BAU: Fixes meursing
    We added a new concept to our caching list
    
    - View fragment caching
    
    This threw me a bunch as I wasn't expecting to be investigating another
    caching concept on top of CDN, redis fragments in the frontend and
    backend fragments plus etags to boot.
    
    TBH I'd probably like to just remove this as we really don't need to
    cache these fragments.

--- app/helpers/cache_helper.rb
+++ app/helpers/cache_helper.rb
@@ -2,4 +2,13 @@ module CacheHelper
   def cache_key
     [TradeTariffFrontend::ServiceChooser.cache_prefix, @tariff_last_updated, TradeTariffFrontend.revision].join('/')
   end
+
+  def commodity_cache_key
+    [
+      'commodities#show',
+      cache_key,
+      meursing_lookup_result.meursing_additional_code_id,
+      params.values.compact.map(&:to_s).sort.join('/'),
+    ].compact
+  end
 end
