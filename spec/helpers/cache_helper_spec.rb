52bc66495ae69fa9539beb5504e1a23f17f647c0
rasika.abeyrathna <rasikasri@gmail.com>
2025-11-28 14:58:45 +0000

    HMRC-1793: CC 8409990035 issue when entering date 4th January 2025 show 1st April 2025 - test

--- spec/helpers/cache_helper_spec.rb
+++ spec/helpers/cache_helper_spec.rb
@@ -1,5 +1,7 @@
 RSpec.describe CacheHelper, type: :helper do
   describe '#commodity_cache_key' do
+    subject(:instance) { cacheable.new('2025-01-01') }
+
     let(:cacheable) do
       Class.new do
         include CacheHelper
@@ -8,28 +10,29 @@ RSpec.describe CacheHelper, type: :helper do
         def initialize(last_updated)
           @tariff_last_updated = last_updated
         end
+
+        def meursing_lookup_result
+          MeursingLookup::Result.new(meursing_additional_code_id: '100')
+        end
       end
     end
-
-    subject(:instance) { cacheable.new("2025-01-01") }
 
     before do
       allow(TradeTariffFrontend::ServiceChooser).to receive(:cache_prefix).and_return('uk')
       allow(TradeTariffFrontend).to receive(:revision).and_return('1')
-      allow(instance).to receive(:meursing_lookup_result).and_return(double(meursing_additional_code_id: "100"))
       allow(instance).to receive(:params).and_return(ActionController::Parameters.new(params_hash))
     end
 
     context 'when date params are available, builds the correct commodity cache key' do
-      let(:params_hash) { {day: '04', month: '01', year: '2025', id: '2008605010'} }
+      let(:params_hash) { { day: '04', month: '01', year: '2025', id: '2008605010' } }
 
-      it { expect(instance.commodity_cache_key).to eq(%W[commodities#show uk/2025-01-01/1 100 04/2008605010/01/2025]) }
+      it { expect(instance.commodity_cache_key).to eq(%w[commodities#show uk/2025-01-01/1 100 04/2008605010/01/2025]) }
     end
 
     context 'when date and month swapped for subsequent requests, cache keys are different' do
-      let(:params_hash) { {day: '01', month: '04', year: '2025', id: '2008605010'} }
+      let(:params_hash) { { day: '01', month: '04', year: '2025', id: '2008605010' } }
 
-      it { expect(instance.commodity_cache_key).to eq(%W[commodities#show uk/2025-01-01/1 100 01/2008605010/04/2025]) }
+      it { expect(instance.commodity_cache_key).to eq(%w[commodities#show uk/2025-01-01/1 100 01/2008605010/04/2025]) }
     end
   end
 end
