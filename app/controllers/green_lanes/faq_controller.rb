2964d4030c7556d60c59ebc9001a78bdcace7dad
Amber Cronin <amber@amber.vision>
2025-12-18 11:00:56 +0000

    HMRC-1438: Remove skip_before_action

--- app/controllers/green_lanes/faq_controller.rb
+++ app/controllers/green_lanes/faq_controller.rb
@@ -1,6 +1,5 @@
 module GreenLanes
   class FaqController < ApplicationController
-    skip_before_action :set_search, only: [:index]
     before_action :check_green_lanes_enabled,
                   :disable_switch_service_banner,
                   :disable_search_form
