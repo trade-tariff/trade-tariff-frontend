97c55f1450a390415da2fa1a07e3aa9dcb3c35b5
Amber Cronin <amber@amber.vision>
2026-02-12 17:18:53 +0000

    HMRC-1809: Remove unused test case
    Removed test for 0 items, which the former implementation did not render
    anything for anyway.

--- spec/views/shared/_derived_goods_nomenclature.html.erb_spec.rb
+++ spec/views/shared/_derived_goods_nomenclature.html.erb_spec.rb
@@ -7,37 +7,14 @@ RSpec.describe 'shared/_derived_goods_nomenclature', type: :view do
     render 'shared/derived_goods_nomenclature', deriving_goods_nomenclatures:
   end
 
-  context 'with 1 item in deriving goods nomanclature array' do
-    let(:deriving_goods_nomenclatures) { build_list :goods_nomenclature, 1 }
-
-    it { is_expected.to have_css 'p', text: /Goods previously classified under/ }
-
-    it { is_expected.to have_css 'p', text: deriving_goods_nomenclatures.first.goods_nomenclature_item_id }
-
-    it { is_expected.to have_css 'p', text: deriving_goods_nomenclatures.first.formatted_description }
-
-    it { is_expected.to have_css 'p', text: deriving_goods_nomenclatures.first.validity_start_date.to_formatted_s(:long) }
-  end
-
-  context 'with more than 1 item in deriving goods nomanclature array' do
+  context 'with 1 or more items in deriving goods nomanclature array' do
     let(:deriving_goods_nomenclatures) { build_list :goods_nomenclature, 2 }
 
     it { is_expected.to have_css 'table' }
-
     it { is_expected.to have_css 'p', text: /Goods previously classified under/ }
-
     it { is_expected.to have_css 'td', text: deriving_goods_nomenclatures.first.goods_nomenclature_item_id }
-
     it { is_expected.to have_css 'td', text: deriving_goods_nomenclatures.first.formatted_description }
-
     it { is_expected.to have_css 'td', text: deriving_goods_nomenclatures.first.validity_start_date.to_formatted_s(:long) }
-
     it { is_expected.to have_css 'td', count: 6 }
-  end
-
-  context 'with 0 items in goods nomanclature array' do
-    let(:deriving_goods_nomenclatures) { [] }
-
-    it { is_expected.not_to have_css 'table' }
   end
 end
