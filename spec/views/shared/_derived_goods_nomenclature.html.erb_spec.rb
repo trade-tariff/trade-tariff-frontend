39ee170031650d70b016252566918675d56cbedd
Amber Cronin <amber@amber.vision>
2026-02-13 08:08:49 +0000

    HMRC-1809: Add guard clause for instances of 0

--- spec/views/shared/_derived_goods_nomenclature.html.erb_spec.rb
+++ spec/views/shared/_derived_goods_nomenclature.html.erb_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'shared/_derived_goods_nomenclature', type: :view do
     render 'shared/derived_goods_nomenclature', deriving_goods_nomenclatures:
   end
 
-  context 'with 1 or more items in deriving goods nomanclature array' do
+  context 'with 1 or more derived goods nomenclatures' do
     let(:deriving_goods_nomenclatures) { build_list :goods_nomenclature, 2 }
 
     it { is_expected.to have_css 'table' }
@@ -16,5 +16,11 @@ RSpec.describe 'shared/_derived_goods_nomenclature', type: :view do
     it { is_expected.to have_css 'td', text: deriving_goods_nomenclatures.first.formatted_description }
     it { is_expected.to have_css 'td', text: deriving_goods_nomenclatures.first.validity_start_date.to_formatted_s(:long) }
     it { is_expected.to have_css 'td', count: 6 }
+  end
+
+  context 'with 0 derived goods nomenclatures' do
+    let(:deriving_goods_nomenclatures) { [] }
+
+    it { is_expected.not_to have_css 'table' }
   end
 end
