75ca5b47101b7cee767eef373a9e0f3ddeefefa5
William Fish <william.michael.fish@gmail.com>
2026-03-05 10:51:11 +0000

    AI-410: Align search form with GOV.UK conditional reveal pattern

--- spec/views/find_commodities/show_interactive.html.erb_spec.rb
+++ spec/views/find_commodities/show_interactive.html.erb_spec.rb
@@ -21,19 +21,46 @@ RSpec.describe 'find_commodities/show_interactive', type: :view do
     it { is_expected.to have_text('Guided search') }
   end
 
+  describe 'conditional reveal content' do
+    it { is_expected.to have_css('textarea#guided_q[name="q"]') }
+    it { is_expected.to have_text('Describe the products you are trading') }
+    it { is_expected.to have_text('Enter the name of the products or commodity code') }
+    it { is_expected.to have_text('For example, tomatoes or 1234 5678 90') }
+  end
+
   describe 'help details components' do
     it { is_expected.to have_css('.govuk-details__summary-text', text: 'What are keyword, commodity code and guided searches?') }
     it { is_expected.to have_css('.govuk-details__summary-text', text: 'Tips for using keyword or commodity code search') }
     it { is_expected.to have_css('.govuk-details__summary-text', text: 'Tips for using guided search') }
   end
 
-  describe 'devolved nations banner' do
-    it { is_expected.to have_css('.govuk-notification-banner__title', text: 'Devolved nations') }
+  describe 'keyword tips content' do
+    it { is_expected.to have_css('.special-numbered-list') }
+    it { is_expected.to have_text('Steps for searching the tariff') }
   end
 
-  describe 'browse/A-Z cards' do
-    it { is_expected.to have_css('.govuk-card') }
-    it { is_expected.to have_link('Browse the goods classification') }
+  describe 'devolved nations banner' do
+    it { is_expected.to have_css('.app-devolved-nations', text: /Applies to England, Scotland and Wales/) }
+    it { is_expected.to have_link('Guidance for Northern Ireland') }
+  end
+
+  describe 'other ways to search' do
+    it { is_expected.to have_css('h2', text: 'Other ways to search for a commodity') }
+    it { is_expected.to have_link('Browse the tariff') }
     it { is_expected.to have_link('A-Z of classified goods') }
+    it { is_expected.to have_text('Browse the goods classification') }
+    it { is_expected.to have_text('Look for your product in the A-Z') }
+  end
+
+  describe 'fieldset legend' do
+    it { is_expected.to have_css('legend h2', text: 'What type of search are you doing?') }
+  end
+
+  describe 'date picker' do
+    it { is_expected.to have_text('When are you planning to trade the products?') }
+  end
+
+  describe 'submit button' do
+    it { is_expected.to have_css('input[type="submit"][value="Search for a commodity"]') }
   end
 end
