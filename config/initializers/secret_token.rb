90f8129f44762d3498ff8ac0b7bbc649bbf3493f
William Fish <william.michael.fish@gmail.com>
2026-03-06 11:42:02 +0000

    Revert "[HMRC-1997] Remove hardcoded default for secret"

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,5 +5,5 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 
-TradeTariffFrontend::Application.config.secret_token = ENV.fetch('SECRET_TOKEN')
-TradeTariffFrontend::Application.config.secret_key_base = ENV.fetch('SECRET_KEY_BASE')
+TradeTariffFrontend::Application.config.secret_token = ENV['SECRET_TOKEN']
+TradeTariffFrontend::Application.config.secret_key_base = ENV.fetch('SECRET_KEY_BASE', 'DRP57/m3nTx1UEQ0qAo0IsCKZgKDvu8W+U/8hTO7jPY=')
