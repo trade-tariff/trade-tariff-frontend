6fef0eb819bef556c8548e76de3ada1855991f47
rasika.abeyrathna <rasikasri@gmail.com>
2026-03-25 22:31:50 +0000

    HMRC:-1965: Remove HTTP fallback and tighten security groups

--- config/puma.rb
+++ config/puma.rb
@@ -32,9 +32,9 @@ threads threads_count, threads_count
 environment ENV['RACK_ENV'] || 'development'
 
 # Explicit HTTP bind,  default is 3000.
-if Rails.env.development?
-  bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 3000)}"
-end
+# if Rails.env.development?
+bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 3000)}"
+# end
 
 # Explicit HTTPS bind
 cert = ENV['SSL_CERT_PEM']&.gsub("\\n", "\n")
