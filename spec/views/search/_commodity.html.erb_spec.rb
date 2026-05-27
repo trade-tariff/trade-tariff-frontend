a7b1aac38a9241e3a31ff04b735b83137a2e2f8c
Andrew Pepler <1190196+vertism@users.noreply.github.com>
2026-05-27 09:46:34 +0100

    Fix link

--- spec/views/search/_commodity.html.erb_spec.rb
+++ spec/views/search/_commodity.html.erb_spec.rb
@@ -1,8 +1,10 @@
 RSpec.describe 'search/_commodity', type: :view do
-  subject { render }
+  subject(:rendered) { render }
+
+  let(:request_id) { 'search-request-123' }
 
   before do
-    assign(:search, build(:search, q: 'test'))
+    assign(:search, build(:search, q: 'test', request_id:))
     allow(view).to receive(:commodity).and_return(commodity)
   end
 
@@ -16,5 +18,11 @@ RSpec.describe 'search/_commodity', type: :view do
     let(:commodity) { build(:commodity, description: 'Horses') }
 
     it { is_expected.to have_css('td.govuk-table__cell > a', text: 'Horses') }
+
+    it 'renders a commodity link with the expected href' do
+      link = Capybara.string(rendered).find('td.commodity-result a', text: 'Horses')
+
+      expect(link[:href]).to eq(commodity_path(commodity, request_id:))
+    end
   end
 end
