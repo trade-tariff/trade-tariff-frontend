278642f6d9730288c5f2bf3b7170480103317699
rasika.abeyrathna <rasikasri@gmail.com>
2026-06-15 16:03:58 +0100

    HMRC-2299: ActionView::Template::Error in frontend

--- config/application.rb
+++ config/application.rb
@@ -67,7 +67,7 @@ module TradeTariffFrontend
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks  core_ext])
+    config.autoload_lib(ignore: %w[assets tasks core_ext])
 
     # Configuration for the application, engines, and railties goes here.
     #
