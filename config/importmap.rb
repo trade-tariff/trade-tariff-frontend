057d91f6d55fbb56665693499a19228925126a0a
Amber Cronin <amber@amber.vision>
2026-07-07 15:51:34 +0100

    fix: run pre-commit

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,12 +4,12 @@ pin 'application'
 pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 
-pin "govuk-frontend" # @6.3.0
+pin 'govuk-frontend' # @6.3.0
 pin 'mark.js' # @8.11.1
 pin 'jquery' # @4.0.0
 pin 'accessible-autocomplete' # @3.0.1
-pin "debounce" # @3.0.0
-pin "js-cookie" # @3.0.8
+pin 'debounce' # @3.0.0
+pin 'js-cookie' # @3.0.8
 
 pin_all_from 'app/javascript/src', to: 'src'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
