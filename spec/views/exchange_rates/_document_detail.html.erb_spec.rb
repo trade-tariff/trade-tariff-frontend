684efb11b298bc13016f43d253a614318bdf0161
William Fish <william.michael.fish@gmail.com>
2026-07-09 16:15:20 +0100

    fix(styles): restore GOV.UK document frame on attachment thumbs
    Replace the pale #f0f0f0 border on .downloads .attachment-thumb with
    GOV.UK-style white background + drop shadow + outline so spreadsheet
    icons read as paper documents again on exchange rates and help pages.
    
    Issue: No ticket/issue

--- spec/views/exchange_rates/_document_detail.html.erb_spec.rb
+++ spec/views/exchange_rates/_document_detail.html.erb_spec.rb
@@ -15,4 +15,14 @@ RSpec.describe 'exchange_rates/_document_detail', type: :view do
   it { is_expected.to have_link('CSV', href: '/exchange_rates/view/files/exrates-monthly-0623.csv', count: 2) }
 
   it { is_expected.to have_link('View online', href: "/exchange_rates/view/#{period.year}-#{period.month}?type=spot") }
+
+  it 'renders the shared downloads attachment-thumb with the spreadsheet icon asset' do
+    render
+
+    expect(rendered).to have_css '.downloads .attachment-thumb img[src*="spreadsheet"]'
+  end
+
+  it { is_expected.to have_css 'p', text: 'This file may not be suitable for users of assistive technology' }
+
+  it { is_expected.to have_css 'details', text: 'Request an accessible format.' }
 end
