211b3d349943055554b004b1c6719ebe8bfbd4a8
William Fish <william.michael.fish@gmail.com>
2026-07-31 15:10:29 +0100

    fix(frontend): drop help-link changes from intercept PR
    Keep only description-intercept commodity code linkification removal.
    
    Issue: No ticket/issue

--- app/helpers/intercept_guidance_helper.rb
+++ app/helpers/intercept_guidance_helper.rb
@@ -6,14 +6,6 @@ module InterceptGuidanceHelper
     'webchat_url' => ->(_) { TradeTariffFrontend.webchat_url },
   }.freeze
 
-  # Legacy destinations for the generic intercept "help on using the tariff" link.
-  # Prefer {{help_url}} in new template copy; keep these rewrites for already-published intercepts.
-  LEGACY_HELP_URLS = [
-    'https://www.gov.uk/guidance/classification-of-goods/',
-    'https://www.gov.uk/guidance/classification-of-goods',
-    '/help/help_find_commodity',
-  ].freeze
-
   GOVUK_MARKDOWN_CLASSES = {
     'p' => %w[govuk-body],
     'a' => %w[govuk-link],
@@ -29,13 +21,8 @@ module InterceptGuidanceHelper
 
     message.to_s.gsub(/\{\{(\w+)\}\}/) do |match|
       key = ::Regexp.last_match(1)
-      case key
-      when 'help_url'
-        help_path
-      else
-        resolver = SUPPORTED_PLACEHOLDERS[key]
-        resolver ? resolver.call(search) : match
-      end
+      resolver = SUPPORTED_PLACEHOLDERS[key]
+      resolver ? resolver.call(search) : match
     end
   end
 
@@ -53,23 +40,13 @@ private
     GOVUK_MARKDOWN_CLASSES.each do |selector, classes|
       fragment.css(selector).each { |node| append_classes(node, classes) }
     end
-    fragment.css('a').each do |node|
-      normalize_legacy_help_link(node)
-      apply_new_tab_standard(node)
-    end
+    fragment.css('a').each { |node| apply_new_tab_standard(node) }
 
     fragment.to_html.html_safe
   end
 
   def append_classes(node, classes)
     node['class'] = (node['class'].to_s.split + classes).uniq.join(' ')
-  end
-
-  def normalize_legacy_help_link(node)
-    href = node['href'].to_s
-    return unless LEGACY_HELP_URLS.include?(href) || href.end_with?('/help/help_find_commodity')
-
-    node['href'] = help_path
   end
 
   def apply_new_tab_standard(node)
