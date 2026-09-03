a6f7d32e64301b3b1c57e9000f5acef9ec294b97
William Fish <william.michael.fish@gmail.com>
2026-09-03 13:00:18 +0100

    fix(requests): treat unknown country as unavailable
    Jira: AI-1259

--- spec/lib/trade_tariff_frontend/request_country_spec.rb
+++ spec/lib/trade_tariff_frontend/request_country_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 RSpec.describe TradeTariffFrontend::RequestCountry do
   describe '.normalize' do
     it { expect(described_class.normalize('GB')).to eq('gb') }
+    it { expect(described_class.normalize('XX')).to eq('') }
     it { expect(described_class.normalize(nil)).to eq('') }
     it { expect(described_class.normalize('not-a-country')).to eq('') }
   end
