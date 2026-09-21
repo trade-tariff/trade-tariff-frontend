bc87466fe17970c810891bfd03f4cd2747a9306d
rasikasri <44141067+rasikasri@users.noreply.github.com>
2026-09-21 17:59:45 +0100

    HMRC-2326: Display Correct Description Based on Time Context (#3375)
    * HMRC-2326: Display Correct Description Based on Time Context

--- app/models/concerns/has_excluded_countries.rb
+++ app/models/concerns/has_excluded_countries.rb
@@ -1,19 +1,29 @@
 module HasExcludedCountries
   extend ActiveSupport::Concern
 
-  def excluded_country_list
-    countries = if exclusions_include_european_union?
+  def excluded_country_list(as_of:)
+    return ''.html_safe if excluded_countries.blank?
+
+    countries = if exclusions_include_european_union?(as_of:)
                   # Replace EU members with the EU geographical_area
-                  [GeographicalArea.european_union] + excluded_countries.delete_if(&:eu_member?)
+                  [GeographicalArea.european_union(as_of:)] + excluded_countries.delete_if { |country| country.eu_member?(as_of:) }
                 else
                   excluded_countries
                 end
 
     countries.map(&:description).sort.join(', ').html_safe
+  rescue Faraday::Error => e
+    Rails.logger.warn "Unable to resolve replace EU exclusion list, as_of=#{as_of}. Error: #{e.message}"
+    excluded_countries.map(&:description).sort.join(', ').html_safe
   end
 
-  def exclusions_include_european_union?
-    GeographicalArea.eu_members_ids.all? { |eu_member| eu_member.in?(excluded_country_ids) }
+  def exclusions_include_european_union?(as_of:)
+    return false if excluded_countries.blank?
+
+    GeographicalArea.eu_members_ids(as_of:).all? { |eu_member| eu_member.in?(excluded_country_ids) }
+  rescue Faraday::Error => e
+    Rails.logger.warn "Failed to fetch EU member countries list, as_of=#{as_of}. Error: #{e.message}"
+    false
   end
 
   private
